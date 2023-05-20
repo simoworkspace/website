48e6f224e4adffe2c892552822f79afdf701e57e
Spyei <caio.salamao1234@gmail.com>
2023-05-20 12:57:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/header/Header';
+import { Body } from './components/body/Body';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    );
+}
+
+export default App;
---
+++ types.d.ts
@@ -0,0 +1,18 @@
+type Snowflake = string;
+
+export interface BotType {
+    _id: Snowflake;
+    name: string;
+    avatar: string;
+    botInviteURL: string;
+    websiteURL: string;
+    supportServer: string;
+    sourceCode: string;
+    description: string;
+    longDescription: string;
+    prefix: string[] | string;
+    owners: Snowflake[] | string;
+    creationTimestamp: number;
+    verifiedBot: boolean;
+    tags: string[];
+}
