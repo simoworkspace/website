ef94ca9d15190734d363931fe6aeea25e744ab0a
Spyei <caio.salamao1234@gmail.com>
2023-06-02 18:25:03 -0300

    improving find bot component + steps component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { Header } from './components/Header';
-import { Bots } from './components/Bots';
-import { Addbot } from './pages/Addbot';
-import { Routes, Route } from 'react-router-dom';
-import { NotFound } from './pages/NotFound';
-import { Mobilemenu } from './components/Mobilemenu';
-import { Bot } from './pages/Bot';
+import { Header } from "./components/Header";
+import { Bots } from "./components/Bots";
+import { Addbot } from "./pages/Addbot";
+import { Routes, Route } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { Mobilemenu } from "./components/Mobilemenu";
+import { Bot } from "./pages/Bot";
 
 function App() {
     return (
-        <html>
+        <>
             <header>
                 <Header />
             </header>
             <body>
                 <Routes>
-                    <Route path='/bot/:botid' element={<Bot />} />
-                    <Route path='/' element={<Bots />} />
-                    <Route path='addbot' element={<Addbot />} />
-                    <Route path='*' element={<NotFound />} />
+                    <Route path="/bot/:botid" element={<Bot />} />
+                    <Route path="/" element={<Bots />} />
+                    <Route path="addbot" element={<Addbot />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
                 <Mobilemenu />
             </body>
-            
-        </html>
+        </>
     );
 }
 
