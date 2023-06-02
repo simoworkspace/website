f3c19dcccd6a64bef7ff7ca9fcc623983574ce4b
Spyei <caio.salamao1234@gmail.com>
2023-06-02 15:15:01 -0300

    adding a form do find bot

---
+++ src/components/FormFindBot.tsx
@@ -0,0 +1,80 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import axios, { AxiosResponse } from "axios";
+import searchIcon from "../assets/svg/search.svg";
+
+export const FormFindBot: React.FC<{
+    setVerificarBot: (value: boolean) => void;
+}> = ({ setVerificarBot }) => {
+    const [statusCode, setStatusCode] = useState<number>();
+    const [id, setId] = useState<string>("");
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setId(event.target.value);
+    };
+
+    const handleSubmit = async (
+        event: FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+        try {
+            event.preventDefault();
+            const res: AxiosResponse = await axios.get(
+                `${import.meta.env.VITE_API_URL}/bot/${id}/discord`,
+                {
+                    headers: {
+                        Authorization: import.meta.env.VITE_API_KEY as string,
+                    },
+                }
+            );
+            res.data.bot ? setVerificarBot(true) : setVerificarBot(false);
+        } catch (error: any) {
+            setStatusCode(error.response.status);
+        }
+    };
+
+    return (
+        <div className="text-white flex items-center justify-center w-[100vw] h-[60vh]">
+            <form
+                className="flex-row flex items-center justify-center h-[90px]"
+                onSubmit={handleSubmit}
+            >
+                <div className="flex items-center flex-col">
+                    <div
+                        className={`border-[#8b8b8b] items-center justify-center h-100px transition-all duration-300 rounded-xl border-2 bg-[#2c2c2c] ${
+                            statusCode === 404
+                                ? "border-[#ff0000]"
+                                : "hover:border-roxo-legal focus-within:border-roxo-legal"
+                        }`}
+                    >
+                        <input
+                            className="bg-transparent p-4 flex items-center justify-center outline-none w-full"
+                            type="text"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div
+                        className={`text-[#ff0000] text-[0px] ${
+                            statusCode === 404 && "text-[16px]"
+                        } transition-all duration-300`}
+                    >
+                        Digite um id válido!
+                    </div>
+                </div>
+                <div className="flex m-2 flex-col">
+                    <button
+                        className={`flex rounded-xl flex-row items-center justify-center bg-[#5353eb] hover:bg-[#2d2dda] ${
+                            statusCode == 404 && "mb-[24px]"
+                        } transition-all focus:bg-[#2d2dda] duration-300 p-4`}
+                        type="submit"
+                    >
+                        <img
+                            className="mr-2"
+                            src={searchIcon}
+                            alt="Search Icon"
+                        />
+                        <span>Encontrar bot</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
--- src/pages/Addbot.tsx
+++ src/pages/Addbot.tsx
@@ -1,46 +1,14 @@
 import { FormAddbot } from "../components/FormAddbot";
-import React, { useState, ChangeEvent, FormEvent } from "react";
-import axios, { AxiosResponse } from "axios";
+import React, { useState } from "react";
+import { FormFindBot } from "../components/FormFindBot";
 
 export const Addbot: React.FC = () => {
-    const [verificarBot, setVerificarBot] = useState<boolean>();
-    const [id, setId] = useState<string>("");
-
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        setId(event.target.value);
-    };
-
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
-        event.preventDefault();
-        const res: AxiosResponse = await axios.get(
-            `${import.meta.env.VITE_API_URL}/bot/${id}/discord`,
-            {
-                headers: {
-                    Authorization: import.meta.env.VITE_API_KEY as string,
-                },
-            }
-        );
-        res.data?.bot ? setVerificarBot(true) : setVerificarBot(false)
-    };
+    const [verificarBot, setVerificarBot] = useState(false);
 
     return verificarBot ? (
-        <div>
-            <div>
-                <FormAddbot />
-            </div>
-        </div>
+        <FormAddbot />
     ) : (
-        <div className="text-white">
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="text-black"
-                    type="text"
-                    onChange={handleInputChange}
-                />
-                <button type="submit">Encontrar bot</button>
-                {!verificarBot ? ( <div>coiso invalido</div> ) : <div></div> }
-            </form>
-        </div>
+        <FormFindBot setVerificarBot={setVerificarBot} />
     );
 };
 
