8eed3e5fa17f374a5c802f75e8445e3e173f7fcd
Spyei <caio.salamao1234@gmail.com>
2023-06-04 13:44:56 -0300

    fixing addbot form bug in mobile

--- src/pages/Addbot.tsx
+++ src/pages/Addbot.tsx
@@ -11,9 +11,7 @@ export const Addbot: React.FC = () => {
 
     return (
         <div className="flex flex-row gap-10 xl:items-center xl:flex-col m-6">
-            <div
-                className={`flex xl:w-[85vw] w-[23vw]`}
-            >
+            <div className={`flex xl:w-[85vw] w-[23vw]`}>
                 {stepsState === 1 ? (
                     <StepsForm setStepsState={setStepsState}>
                         <img
@@ -34,8 +32,8 @@ export const Addbot: React.FC = () => {
             </div>
             <div
                 className={`flex self-center xl:w-[90vw] ${
-                    stepsState === 2 && "xl:h-[102rem] xl:mb-12"
-                } xl:h-[300px] w-[70vw]`}
+                    stepsState === 2 ? "xl:h-[102rem] xl:mb-12" : "xl:h-[300px]"
+                } w-[70vw]`}
             >
                 <div className="flex p-3 rounded-3xl justify-center items-center h-[100%] w-[100%] bg-neutral-900 shadow-md shadow-black text-white">
                     {verificarBot ? (
