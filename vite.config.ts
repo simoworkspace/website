78440d3301c5125bddf5f0aec7832b207c5778a8
Spyei <caio.salamao1234@gmail.com>
2023-06-26 14:53:53 -0300

    proxy again :sob:

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
     server: {
@@ -7,7 +7,7 @@ export default defineConfig({
         // port: 80,
         proxy: {
             "/api": {
-                target: "http://localhost:80",
+                target: "https://botlist-api.squareweb.app",
                 changeOrigin: true,
                 secure: false,
                 ws: true,
