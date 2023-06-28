68f034e41cf6249c28d46c6a535893e7f5b7c9c7
Spyei <caio.salamao1234@gmail.com>
2023-06-28 10:58:07 -0300

    fixing auth method

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 interface Props {
     children: React.ReactNode;
@@ -7,10 +8,21 @@ export const RequireAuth: React.FC<Props> = ({ children }) => {
     const [auth, setAuth] = useState<boolean>(true);
 
     useEffect(() => {
-        const getUserData = (): void => {
-            const userCookie: string | undefined =
-                document.cookie.split("discordUser=")[1];
-            userCookie ? setAuth(true) : setAuth(false);
+        const getUserData = async () => {
+            const getUserData = () => {
+                axios.get("/api/auth/user", {
+                        withCredentials: true,
+                        headers: {
+                            Authorization: import.meta.env.VITE_API_KEY,
+                        },
+                    })
+                    .then((res) => {
+                        setAuth(true)
+                    }).catch((error) => {
+                        setAuth(false)
+                    })
+            };
+            getUserData();
         };
 
         getUserData();
