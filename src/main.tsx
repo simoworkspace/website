a7368a5d7a4fb85a39aa714cbf0afdabcece6c67
Spyei <caio.salamao1234@gmail.com>
2023-06-29 14:04:25 -0300

    main.tsx -> App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Addbot } from "./pages/Addbot";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
+import { Mobilemenu } from "./components/Mobilemenu";
+import { Bot } from "./pages/Bot";
+import { RequireAuth } from "./components/RequireAuth";
+import { Guild } from "./pages/Guild";
+import { Vote } from "./pages/Vote";
+import { Guilds } from "./pages/Guilds";
+import { Tests } from "./pages/Tests";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
 import "./index.css";
 import "tailwindcss/tailwind.css";
+
+function App() {
+    return (
+        <main className="overflow-x-hidden">
+            <header>
+                <Header />
+            </header>
+            <section>
+                <Routes>
+                    <Route path="/guild/:guildid" element={<Guild />} />
+                    <Route path="/testes" element={<Tests />} />
+                    <Route path="/bot/:botid" element={<Bot />} />
+                    <Route path="/vote/:botid" element={<Vote />} />
+                    <Route path="/guilds" element={<Guilds />} />
+                    <Route path="/" element={<Main />} />
+                    <Route
+                        path="addbot"
+                        element={
+                            <RequireAuth>
+                                <Addbot />
+                            </RequireAuth>
+                        }
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+                <Mobilemenu />
+            </section>
+        </main>
+    );
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
