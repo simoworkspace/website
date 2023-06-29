5c640c42ab3d0fbfef80d2620b2ffe0b9e8c9bf0
Spyei <caio.salamao1234@gmail.com>
2023-06-29 15:47:16 -0300

    fixing providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
 import "./index.css";
 import "tailwindcss/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <UserProvider>
+                <App />
+            </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
