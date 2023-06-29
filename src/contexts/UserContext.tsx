8f469f509054642298dcf0a804d638bfccc09625
Spyei <caio.salamao1234@gmail.com>
2023-06-29 14:54:35 -0300

    fixing contexts to signed user

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useEffect } from "react";
+import { UserStructure } from "../types";
+import api from "../api";
+import { AxiosResponse } from "axios";
+
+interface UserContextProps {
+    user: UserStructure | null;
+    setUser: (user: UserStructure | null) => void;
+}
+
+export const UserContext = createContext<UserContextProps>({
+    user: null,
+    setUser: () => {},
+});
+
+export const UserProvider: React.FC<{ children: any }> = ({
+    children,
+}: React.PropsWithChildren) => {
+    const [user, setUser] = useState<UserStructure | null>(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const res: AxiosResponse<{ data: UserStructure }> =
+                await api.getUserData();
+            setUser(res.data.data);
+        };
+
+        fetchUserData();
+    }, []);
+
+    return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
