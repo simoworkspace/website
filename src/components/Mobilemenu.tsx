a1139b4fd6b9cbfc24b61fd76159c83027b04eb8
Spyei <caio.salamao1234@gmail.com>
2023-07-01 10:55:19 -0300

    adding login and logout to mobilemenu

--- src/components/Mobilemenu.tsx
+++ src/components/Mobilemenu.tsx
@@ -2,53 +2,69 @@ import { Link } from "react-router-dom";
 import searchIcon from "../assets/svgs/search.svg";
 import plusIcon from "../assets/svgs/plus.svg";
 import serversIcon from ".././assets/svgs/servers.svg";
-import React from "react";
-const avatarImage: string =
-    "https://cdn.discordapp.com/avatars/955095844275781693/511a594d8af5dd14849cc3e16567f534.png?size=2048";
+import React, { useContext } from "react";
+import { UserStructure } from "../types";
+import { UserContext } from "../contexts/UserContext";
+import api from "../api";
 
 export const Mobilemenu: React.FC = () => {
+    const { user } = useContext<UserStructure | any>(UserContext);
+
     return (
         <div className="hidden xl:fixed xl:bottom-0 xl:left-0 xl:w-full xl:bg-gray-900 xl:text-white xl:py-3 xl:flex xl:justify-around xl:items-center">
-            <div className="flex flex-col items-center">
-                <Link to="/">
-                    <img
-                        src={searchIcon}
-                        alt="Ícone 1"
-                        className="h-6 w-6 text-white"
-                    />
-                </Link>
-                <span className="text-xs">Procurar bot</span>
-            </div>
-            <div className="flex flex-col items-center">
-                <Link to="/addbot">
-                    <img
-                        src={plusIcon}
-                        alt="Ícone 2"
-                        className="h-6 w-6 text-white"
-                    />
-                </Link>
-                <span className="text-xs">Adicionar bot</span>
-            </div>
-            <div className="flex flex-col items-center">
-                <Link to="/">
-                    <img
-                        src={serversIcon}
-                        alt="Ícone 3"
-                        className="h-6 w-6 text-white"
-                    />
-                </Link>
-                <span className="text-xs">Servidores</span>
-            </div>
-            <div className="flex flex-col items-center">
-                <Link to="/">
-                    <img
-                        className="h-6 w-6 text-white rounded-full"
-                        src={avatarImage}
-                        alt="User Avatar"
-                    />
-                </Link>
-                <span className="text-xs">Perfil</span>
-            </div>
+            {user ? (
+                <>
+                    <div className="flex flex-col items-center">
+                        <Link to="/">
+                            <img
+                                src={searchIcon}
+                                alt="Ícone 1"
+                                className="h-6 w-6 text-white"
+                            />
+                        </Link>
+                        <span className="text-xs">Procurar bot</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Link to="/addbot">
+                            <img
+                                src={plusIcon}
+                                alt="Ícone 2"
+                                className="h-6 w-6 text-white"
+                            />
+                        </Link>
+                        <span className="text-xs">Adicionar bot</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Link to="/">
+                            <img
+                                src={serversIcon}
+                                alt="Ícone 3"
+                                className="h-6 w-6 text-white"
+                            />
+                        </Link>
+                        <span className="text-xs">Servidores</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <button
+                            onClick={async () => {
+                                await api.logoutUser();
+                                return window.location.reload();
+                            }}
+                        >
+                            <img
+                                className="h-6 w-6 text-white rounded-full"
+                                src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=2048`}
+                                alt="User Avatar"
+                            />
+                        </button>
+                        <span className="text-xs">Logout</span>
+                    </div>
+                </>
+            ) : (
+                <div className="w-[100vw] flex items-center justify-center">
+                    <Link className="w-[100%] self-center text-center" to={import.meta.env.VITE_AUTH_LINK}>login</Link>
+                </div>
+            )}
         </div>
     );
 };
