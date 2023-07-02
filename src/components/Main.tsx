0b7e697a659fe5b565a18da8f470824af79785d3
Spyei <caio.salamao1234@gmail.com>
2023-07-02 11:28:00 -0300

    fixing bots buttons for mobile

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,7 +13,9 @@ export const Main: React.FC = () => {
         setData(res.data);
     };
 
-    useEffect(() => { fetchData() }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return data ? (
         <div className="grid-cols-2 grid gap-8 text-white m-2 xl:grid-cols-1 xl:items-left xl:justify-left">
@@ -21,7 +23,7 @@ export const Main: React.FC = () => {
                 <Link
                     to={`/bot/${bot._id}`}
                     key={bot._id}
-                    className="bg-neutral-950 duration-300 transition-colors hover:bg-neutral-900 shadow-md shadow-black p-3 border-white border-2 bg-neutral-940 rounded-lg"
+                    className="bg-neutral-950 duration-300 transition-colors hover:bg-neutral-900 shadow-md shadow-black p-3 border-white border-2 bg-neutral-940 rounded-lg xl:h-[220px]"
                 >
                     <img
                         className="w-[min(100%,100px)] h-[min(100%,100px)] rounded-full mt-2 mr-2 float-right"
@@ -35,22 +37,18 @@ export const Main: React.FC = () => {
                             : bot.shortDescription}
                     </div>
                     <div className="mt-2 ml-2 mb-2 gap-2 flex flex-row">
-                        <div>
-                            <Link
-                                to={`https://discord.com/api/oauth2/authorize?client_id=${bot._id}&permissions=70368744177655&scope=bot%20applications.commands`}
-                            >
-                                <button className="border-2 border-neutral-700 bg-neutral-900 hover:bg-neutral-700 transition-colors duration-300 rounded-lg max-w-full max-h-full w-[170px] h-[40px]">
-                                    Adicionar
-                                </button>
-                            </Link>
-                        </div>
-                        <div>
-                            <Link to={`/vote/${bot._id}`}>
-                                <button className="border-2 border-neutral-700 bg-neutral-900 hover:bg-neutral-700 transition-colors duration-300 rounded-lg max-w-full max-h-full w-[170px] h-[40px]">
-                                    Votar
-                                </button>
-                            </Link>
-                        </div>
+                        <Link
+                            to={`https://discord.com/api/oauth2/authorize?client_id=${bot._id}&permissions=70368744177655&scope=bot%20applications.commands`}
+                        >
+                            <button className="border-2 border-neutral-700 bg-neutral-900 hover:bg-neutral-700 transition-colors duration-300 rounded-lg max-w-full max-h-full xl:w-[140px] w-[170px] h-[40px]">
+                                Adicionar
+                            </button>
+                        </Link>
+                        <Link to={`/vote/${bot._id}`}>
+                            <button className="border-2 border-neutral-700 bg-neutral-900 hover:bg-neutral-700 transition-colors duration-300 rounded-lg max-w-full max-h-full xl:w-[140px] w-[170px] h-[40px]">
+                                Votar
+                            </button>
+                        </Link>
                     </div>
                 </Link>
             ))}
