d326e134872026a5edf8ab286589fabd13514ca6
Caio <105593678+Spyei@users.noreply.github.com>
2023-07-05 01:05:52 -0300

    netlify test

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import "./index.css";
 import "tailwindcss/tailwind.css";
 import { useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContext";
-
+ // a
 function App() {
     const { color } = useContext(ThemeContext);
 
