f7ed6755aea2343ee241879f4454bffe95ee74d7
Spyei <caio.salamao1234@gmail.com>
2023-07-07 11:47:43 -0300

    fix: Divs positions

--- src/components/Botloading.tsx
+++ src/components/Botloading.tsx
@@ -1,7 +1,7 @@
 export const Botloading: React.FC = () => {
     return (
-        Array(6).fill(
-            <div className="grid-cols-2 gap-8 m-2 grid xl:grid-cols-1">
+        <div className="grid-cols-2 gap-8 m-2 grid xl:grid-cols-1">
+            {Array(6).fill(
                 <div className="border-2 bg-card-blue rounded-[10px]">
                     <div className="bg-neutral-900 xl:w-[80px] xl:h-[80px] w-[100px] h-[100px] float-right rounded-full mt-[10px] mr-[10px]"></div>
                     <div className="bg-neutral-900 xl:w-[200px] w-[220px] h-[26px] rounded-[10px] flex ml-10"></div>
@@ -11,7 +11,7 @@ export const Botloading: React.FC = () => {
                         <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-[20px]"></div>
                     </div>
                 </div>
-            </div>
-        )
+            )}
+        </div>
     );
 };
