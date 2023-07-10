1c74ba621899f32a852ec2d3922c38987a34512d
Caio <105593678+Spyei@users.noreply.github.com>
2023-07-10 00:01:33 -0300

    chore: Better coding

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = document.getElementById("root") as HTMLElement;
+
+createRoot(root).render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider>
