f6708ed0e3080b436199e6f385fa306ed282bf56
Spyei <caio.salamao1234@gmail.com>
2023-07-12 13:17:36 -0300

    feat: New property "margin" added

--- src/components/Colors/Choice.tsx
+++ src/components/Colors/Choice.tsx
@@ -6,9 +6,10 @@ interface ButtonProps {
     name: string;
     color: string;
     theme: Theme;
+    margin?: string
 };
 
-export const ChoiceColor: React.FC<ButtonProps> = ({ name, color, theme }) => {
+export const ChoiceColor: React.FC<ButtonProps> = ({ name, color, theme, margin }) => {
     const { changeTheme } = useContext<ThemeContextProps>(ThemeContext);
 
     const toggleTheme = (newTheme: Theme) => {
@@ -16,12 +17,12 @@ export const ChoiceColor: React.FC<ButtonProps> = ({ name, color, theme }) => {
     };
 
     return (
-        <button onClick={() => toggleTheme(theme)} className="hover:bg-[#3a3a3a] rounded-md m-[6px] transition-all">
+        <button onClick={() => toggleTheme(theme)} className={`hover:bg-[#3a3a3a] rounded-md m-[${margin || "6px"}] transition-all`}>
             <div className="flex gap-2 flex-row items-center justify-center">
                 <div className="flex items-center justify-start">
                     <div className={`w-3 h-3 bg-[${color}] rounded-full justify-start`}></div>
                 </div>
-                <div className="flex items-center justify-center w-[100%]">
+                <div className="flex items-center justify-center w-full">
                     <div>{name}</div>
                 </div>
             </div>
