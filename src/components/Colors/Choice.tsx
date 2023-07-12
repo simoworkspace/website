40fb56a122a8a33ea3d0240def81f8683a48e07f
Spyei <caio.salamao1234@gmail.com>
2023-07-12 13:26:57 -0300

    fix: Tailwind is a shit

--- src/components/Colors/Choice.tsx
+++ src/components/Colors/Choice.tsx
@@ -4,12 +4,11 @@ import { useContext } from "react";
 
 interface ButtonProps {
     name: string;
-    color: string;
     theme: Theme;
     margin?: string
 };
 
-export const ChoiceColor: React.FC<ButtonProps> = ({ name, color, theme, margin }) => {
+export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin }) => {
     const { changeTheme } = useContext<ThemeContextProps>(ThemeContext);
 
     const toggleTheme = (newTheme: Theme) => {
@@ -17,10 +16,14 @@ export const ChoiceColor: React.FC<ButtonProps> = ({ name, color, theme, margin 
     };
 
     return (
-        <button onClick={() => toggleTheme(theme)} className={`hover:bg-[#3a3a3a] rounded-md m-[${margin || "6px"}] transition-all`}>
+        <button onClick={() => toggleTheme(theme)} className={`hover:bg-[#3a3a3a] rounded-md ${margin === "6px" ? "m-[6px]" : "m-0"} transition-all`}>
             <div className="flex gap-2 flex-row items-center justify-center">
                 <div className="flex items-center justify-start">
-                    <div className={`w-3 h-3 bg-[${color}] rounded-full justify-start`}></div>
+                    <div className={`w-3 h-3 
+                        ${theme === "blue" && "bg-[#004d7c]"} 
+                        ${theme === "green" && "bg-[#04484d]"} 
+                        ${theme === "red" && "bg-[#802222]"} 
+                        ${theme === "purple" && "bg-[#2B195C]"} rounded-full justify-start`}></div>
                 </div>
                 <div className="flex items-center justify-center w-full">
                     <div>{name}</div>
