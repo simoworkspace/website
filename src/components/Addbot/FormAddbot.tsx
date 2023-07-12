7986dfc6683cc640bc978fc10a408e5821714b25
Spyei <caio.salamao1234@gmail.com>
2023-07-12 11:09:56 -0300

    fix: Prose in markdown with tailwindcss/typography

--- src/components/Addbot/FormAddbot.tsx
+++ src/components/Addbot/FormAddbot.tsx
@@ -20,7 +20,46 @@ export const FormAddbot: React.FC = () => {
     const renderMarkdown = () => {
         const processedMarkdown = markdown.replace(/\n/g, '  \n');
         return (
-            <ReactMarkdown>{processedMarkdown}</ReactMarkdown>
+            <ReactMarkdown className="
+            prose
+            prose-p:before:content-none
+            prose-p:after:content-none
+            prose-code:before:content-none
+            prose-code:after:content-none
+            prose-code:p-1
+            prose-code:rounded-md
+            prose-hr:text-white
+            prose-lead:text-white
+            prose-blockquote:text-white
+            prose-headings:text-white
+            prose-h1:text-white
+            prose-h2:text-white
+            prose-h3:text-white
+            prose-h4:text-white
+            prose-h5:text-white
+            prose-h6:text-white
+            prose-p:text-white
+            prose-a:text-blue-500
+            prose-a:hover:text-blue-700
+            prose-a:transition-colors
+            prose-a:duraton-300
+            prose-figure:text-white
+            prose-figcaption:text-white
+            prose-strong:text-white
+            prose-em:text-white
+            prose-code:text-white
+            prose-pre:text-white
+            prose-ol:text-white
+            prose-ul:text-white
+            prose-li:text-white
+            prose-table:text-white
+            prose-thead:text-white
+            prose-tr:text-white
+            prose-th:text-white
+            prose-td:text-white
+            prose-img:text-white
+            prose-video:text-white
+            ">{processedMarkdown}</ReactMarkdown>
         );
     };
 
@@ -95,7 +134,6 @@ export const FormAddbot: React.FC = () => {
                                             cols={22}
                                             name="longDescription"
                                             className="bg-transparent outline-none w-[100%]"
-
                                         />
                                     </div>
                                     {renderMarkdown()}
