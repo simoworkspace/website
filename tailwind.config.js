029cccbdfeb52756b2a9b6fe70464b3fa15a0fec
Spyei <caio.salamao1234@gmail.com>
2023-07-12 13:29:50 -0300

    i hate tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
     theme: {
-        extend: {
-            colors: {
-                "bg-azul": "#012236",
-                "bg-verde": "#04484d",
-                "bg-vermelho": "#380e17",
-                "skeleton-blue": "#24313d",
-                "blue-button1": "#202020",
-                "blue-button2": "#344057",
-                "blue-button3": "#313131",
-                "roxo-legal": "#6565cf",
-            },
-            screens: {
-                xl: { max: "950px" },
-                xp: { max: "460px" },
-            },
-            keyframes: {
-                bganimation: {
-                    "0%": "background-position: left",
-                    "100%": "background-position: right",
-                },
-            },
-        },
+        screens: {
+            xl: { max: "950px" },
+            xp: { max: "460px" },
+        }
     },
     plugins: [require("tailwind-scrollbar"), require("@tailwindcss/typography")],
 };
