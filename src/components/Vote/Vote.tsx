07714441a422116f735a2c1769036d5ad07fc42d
Spyei <caio.salamao1234@gmail.com>
2023-07-16 11:08:14 -0300

    style: Fixing vote message for no login users

--- src/components/Vote/Vote.tsx
+++ src/components/Vote/Vote.tsx
@@ -60,7 +60,7 @@ export const VoteComponent: React.FC = () => {
                         alt={`${discordBotData?.username}'s Avatar`}
                     />
                     <div>
-                        <h1 className="flex">Votar em {discordBotData?.username}</h1>
+                        <h1 className="flex text-2xl">Votar em {discordBotData?.username}</h1>
                         <span>Votos: {votes}</span>
                     </div>
                 </div>
@@ -85,6 +85,34 @@ export const VoteComponent: React.FC = () => {
             <VoteLoading />
         )
     ) : (
-        <div className="text-white">você precisa logar para votar</div>
+        <section className="text-white p-3 w-[100vw] flex flex-col items-center justify-center">
+            <div className="flex flex-row p-3 rounded-lg items-center gap-3">
+                <img
+                    className="w-[100px] rounded-full"
+                    src={`https://cdn.discordapp.com/avatars/${discordBotData?.id}/${discordBotData?.avatar}.png?size=2048`}
+                    alt={`${discordBotData?.username}'s Avatar`}
+                />
+                <div>
+                    <h1 className="flex text-2xl">Votar em {discordBotData?.username}</h1>
+                    <span>Votos: {votes}</span>
+                </div>
+            </div>
+            <div className="flex border-2 w-[60vw] h-[80px] flex-row rounded-lg items-center bg-neutral-900">
+                <span className="text-[20px] w-[82%] ml-3">Você precisar estar logado para poder votar.</span>
+                <div className="flex justify-end">
+                    <button
+                        className={`transition-all duration-300 border-2 rounded-xl w-[100px] h-[50px] disabled:opacity-40
+                    ${color === "blue" && "bg-blue-900 hover:bg-blue-500 border-blue-500 disabled:hover:bg-blue-900"} 
+                    ${color === "green" && "bg-green-900 hover:bg-green-700 border-green-700 disabled:hover:bg-green-900"} 
+                    ${color === "red" && "bg-red-900 hover:bg-red-500 border-red-500 disabled:hover:bg-red-900"}
+                    ${color === "purple" && "bg-purple-900 hover:bg-purple-500 border-purple-500 disabled:hover:bg-purple-900"}
+                    ${color === "black" && "bg-black hover:bg-neutral-900 disabled:bg-neutral-700"}
+                    `}
+                        disabled
+                        onClick={handleVote}
+                    >Votar</button>
+                </div>
+            </div>
+        </section>
     )
 };
