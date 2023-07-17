040b1331ffe5d1846b05613adf7e39227ea09148
Spyei <caio.salamao1234@gmail.com>
2023-07-17 11:36:04 -0300

    feat: Property "selected" added

--- src/components/Colors/Choice.tsx
+++ src/components/Colors/Choice.tsx
@@ -1,30 +1,34 @@
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { ThemeContextProps, Theme } from "../../types";
+import { ThemeContextProps, Theme, ThemeStructure } from "../../types";
 import { useContext } from "react";
 
 interface ButtonProps {
     name: string;
     theme: Theme;
-    margin?: string
+    margin?: string;
+    selected: boolean;
 };
 
-export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin }) => {
+export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin, selected }) => {
     const { changeTheme } = useContext<ThemeContextProps>(ThemeContext);
+
+    const themesOptions: ThemeStructure = {
+        black: "bg-[#2e2e2e]",
+        blue: "bg-[#004d7c]",
+        green: "bg-[#04484d]",
+        purple: "bg-[#2B195C]",
+        red: "bg-[#802222]"
+    }
 
     const toggleTheme = (newTheme: Theme) => {
         changeTheme(newTheme);
     };
 
     return (
-        <button onClick={() => toggleTheme(theme)} className={`hover:bg-[#3a3a3a] rounded-md ${margin === "6px" ? "m-[6px]" : "m-0"} transition-all`}>
+        <button onClick={() => toggleTheme(theme)} className={`${selected ? "bg-[#3a3a3a]" : null} hover:bg-[#3a3a3a] rounded-md ${margin === "6px" ? "m-[6px]" : "m-0"} transition-all`}>
             <div className="flex gap-2 flex-row items-center justify-center">
                 <div className="flex items-center justify-start">
-                    <div className={`w-3 h-3 
-                        ${theme === "blue" && "bg-[#004d7c]"} 
-                        ${theme === "green" && "bg-[#04484d]"} 
-                        ${theme === "red" && "bg-[#802222]"} 
-                        ${theme === "purple" && "bg-[#2B195C]"}
-                        ${theme === "black" && "bg-[#2e2e2e]"} rounded-full justify-start`}></div>
+                    <div className={`w-3 h-3 ${themesOptions[theme]} rounded-full justify-start`}></div>
                 </div>
                 <div className="flex items-center justify-center w-full">
                     <div>{name}</div>
