7bb173e64a54deef1887c26064f8f64b4185353e
Spyei <caio.salamao1234@gmail.com>
2023-07-18 16:27:56 -0300

    fix: Fixing login button width

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 export const LoginButton: React.FC = () => {
     return (
-        <div className="flex text-white w-[140px] h-[50px] xl:hidden">
+        <div className="flex text-white w-[200px] h-[50px] xl:hidden">
             <a className="flex text-white items-center h-[100%] w-[80%] justify-center border-[#858585] border-[1px] bg-black p-6 transition-colors duration-300 hover:bg-neutral-950 hover:border-white" href={import.meta.env.VITE_AUTH_LINK as string}>
                 <div>Login</div>
             </a>
