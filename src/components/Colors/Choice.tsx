49113f21a266f2a50376fb51965cb44eca40a4c1
Spyei <caio.salamao1234@gmail.com>
2023-07-18 10:34:18 -0300

    feat: New option "mobile" for choices component

--- src/components/Colors/Choice.tsx
+++ src/components/Colors/Choice.tsx
@@ -7,9 +7,10 @@ interface ButtonProps {
     theme: Theme;
     margin?: string;
     selected: boolean;
+    mobile?: boolean;
 };
 
-export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin, selected }) => {
+export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin, selected, mobile }) => {
     const { changeTheme } = useContext<ThemeContextProps>(ThemeContext);
 
     const themesOptions: ThemeStructure = {
@@ -24,7 +25,18 @@ export const ChoiceColor: React.FC<ButtonProps> = ({ name, theme, margin, select
         changeTheme(newTheme);
     };
 
-    return (
+    return mobile ? (
+        <button onClick={() => toggleTheme(theme)} className={`${selected ? "bg-[#3a3a3a]" : null} hover:bg-[#3a3a3a] rounded-md ${margin === "6px" ? "m-[6px]" : "m-0"} transition-all`}>
+            <div className="flex gap-2 flex-row items-center justify-center w-[120px] h-[27px]">
+                <div className="flex items-center justify-start">
+                    <div className={`w-3 h-3 ${themesOptions[theme]} rounded-full justify-start`}></div>
+                </div>
+                <div className="flex items-center justify-center w-full">
+                    <div>{name}</div>
+                </div>
+            </div>
+        </button>
+    ) : (
         <button onClick={() => toggleTheme(theme)} className={`${selected ? "bg-[#3a3a3a]" : null} hover:bg-[#3a3a3a] rounded-md ${margin === "6px" ? "m-[6px]" : "m-0"} transition-all`}>
             <div className="flex gap-2 flex-row items-center justify-center">
                 <div className="flex items-center justify-start">
