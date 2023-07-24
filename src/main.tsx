0173ed998a36d675c0750220d9504464d3ba265b
Spyei <caio.salamao1234@gmail.com>
2023-07-24 14:56:43 -0300

    fix: Fixing imports to tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/UserContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./index.css";
+import "tailwindcss/tailwind.css";
 
 const root = document.getElementById("root") as HTMLElement;
 
