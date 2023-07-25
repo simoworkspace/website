3ee09926ad1ac4bd50720afa0324967c8f2cf032
Spyei <caio.salamao1234@gmail.com>
2023-07-25 16:33:30 -0300

    feat: Implementing feedback cards to feedbackCards components

--- src/components/Feedbacks/FeedbackCard.tsx
+++ src/components/Feedbacks/FeedbackCard.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { FeedbackStructure } from "../../types";
+import starIcon from "../../assets/svgs/star.svg";
+import starIconFill from '../../assets/svgs/starfill.svg';
 
-export const FeedbackCard: React.FC = () => {
+export const FeedbackCard: React.FC<{ feedback: FeedbackStructure }> = ({ feedback }) => {
     return (
-        <div>oi</div>
+        <div className="h-[150px] bg-neutral-900 rounded-lg p-1 text-white break-words">
+            <div className="flex flex-col">
+                <div className="flex flex-row items-center m-1 justify">
+                    <img
+                        src={`https://cdn.discordapp.com/avatars/${feedback?.author.id}/${feedback?.author.avatar}.png?size=2048`}
+                        alt={`${feedback?.author.username}'s avatar`}
+                        className="w-[30px] h-[30px] rounded-full"
+                    />
+                    <span className="p-1">{feedback?.author.username}</span>
+                </div>
+                <div className="p-2">{feedback.content}</div>
+                <div className="flex flex-row">
+                    {Array(feedback.stars).fill(0).map(() => (
+                        <img src={starIconFill} alt="Star" />
+                    ))}
+                    {Array(5 - feedback.stars).fill(0).map((_, index) => (
+                        <img key={index + feedback.stars} src={starIcon} alt="Empty Star" />
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/Feedbacks/Feedbacks.tsx
+++ src/components/Feedbacks/Feedbacks.tsx
@@ -1,15 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FeedbackStructure } from "../../types";
 import api from "../../utils/api";
 import { useParams } from "react-router-dom";
-import { UserContext } from "../../contexts/UserContext";
-import starIcon from "../../assets/svgs/star.svg";
-import starIconFill from '../../assets/svgs/starfill.svg';
+import { FeedbackCard } from "./FeedbackCard";
 
 export const Feedbacks: React.FC = () => {
     const [feedbacks, setFeedbacks] = useState<FeedbackStructure[]>();
     const params = useParams();
-    const { user } = useContext(UserContext);
 
     const getBotFeedbacks = async (): Promise<void> => {
         const res = await api.getBotFeedbacks(params.botid as string);
@@ -20,29 +17,7 @@ export const Feedbacks: React.FC = () => {
 
     return (
         <div className="w-[500px] flex flex-col gap-2">
-            {feedbacks?.map(feedback => (
-                <div className="h-[150px] bg-neutral-900 rounded-lg p-1 text-white break-words">
-                    <div className="flex flex-col">
-                        <div className="flex flex-row items-center m-1 justify">
-                            <img
-                                src={`https://cdn.discordapp.com/avatars/${feedback?.author.id}/${feedback?.author.avatar}.png?size=2048`}
-                                alt={`${feedback?.author.username}'s avatar`}
-                                className="w-[30px] h-[30px] rounded-full"
-                            />
-                            <span className="p-1">{feedback?.author.username}</span>
-                        </div>
-                        <div className="p-2">{feedback.content}</div>
-                        <div className="flex flex-row">
-                            {Array(feedback.stars).fill(0).map(() => (
-                                <img src={starIconFill} alt="Star" />
-                            ))}
-                            {Array(5 - feedback.stars).fill(0).map((_, index) => (
-                                <img key={index + feedback.stars} src={starIcon} alt="Empty Star" />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            ))}
+            {feedbacks?.map(feedback => (<FeedbackCard feedback={feedback}/>))}
         </div>
     )
 };
