1437ad0bd7cb3efc666c4214dd3e07ca992c7bff
Spyei <caio.salamao1234@gmail.com>
2023-07-26 11:36:45 -0300

    style: Adding space to stars in feedback

--- src/components/Feedbacks/FeedbackCard.tsx
+++ src/components/Feedbacks/FeedbackCard.tsx
@@ -5,9 +5,9 @@ import starIconFill from '../../assets/svgs/starfill.svg';
 
 export const FeedbackCard: React.FC<{ feedback: FeedbackStructure }> = ({ feedback }) => {
     return (
-        <div className="h-[150px] bg-neutral-900 rounded-lg p-1 text-white break-words">
-            <div className="flex flex-col">
-                <div className="flex flex-row items-center m-1 justify">
+        <div className="bg-neutral-900 rounded-lg p-1 text-white break-words">
+            <div className="flex flex-col p-3 gap-2">
+                <div className="flex flex-row items-center justify">
                     <img
                         src={`https://cdn.discordapp.com/avatars/${feedback?.author.id}/${feedback?.author.avatar}.png?size=2048`}
                         alt={`${feedback?.author.username}'s avatar`}
@@ -15,8 +15,8 @@ export const FeedbackCard: React.FC<{ feedback: FeedbackStructure }> = ({ feedba
                     />
                     <span className="p-1">{feedback?.author.username}</span>
                 </div>
-                <div className="p-2">{feedback.content}</div>
-                <div className="flex flex-row">
+                <div className="py-2">{feedback.content}</div>
+                <div className="flex flex-row gap-1">
                     {Array(feedback.stars).fill(0).map(() => (
                         <img src={starIconFill} alt="Star" />
                     ))}
