9cb9b3c752ba02ae34a3ddc49064c31f6a449bf7
Spyei <caio.salamao1234@gmail.com>
2023-08-15 18:07:19 -0300

    feat: New theme option do dropdownmenu

--- src/components/DropdownMenu/Option.tsx
+++ src/components/DropdownMenu/Option.tsx
@@ -8,13 +8,14 @@ interface Props {
     type: "button" | "link";
     mobile?: boolean;
     alt: string;
-    action?: () => void | Promise<void>;
-    fix?: boolean
+    action?: () => void | JSX.Element |Promise<void | Element>;
+    fix?: boolean;
+    selected?: boolean;
 };
 
-export const MenuOption: React.FC<Props> = ({ icon, title, to, alt, action, type, fix, mobile }) => {
+export const MenuOption: React.FC<Props> = ({ icon, title, to, alt, action, type, fix, mobile, selected }) => {
     return mobile ? (type === "button" ? (
-        <button onClick={action} className="hover:bg-neutral-700 flex flex-row items-center justify-center gap-2 rounded-lg bg-black w-full h-[30px]">
+        <button onClick={action} className="hover:bg-neutral-700 flex flex-row items-center justify-center gap-2 rounded-lg bg-black">
             <img src={icon} alt={alt} />
             <span>{title}</span>
             <img className="w-[20px] -rotate-90" src={arrowIcon} alt="Arrow Icon" />
@@ -28,14 +29,14 @@ export const MenuOption: React.FC<Props> = ({ icon, title, to, alt, action, type
     )
     ) :
         (type === "link" ? (
-            <Link to={to || "/"} className="p-1 text-white flex justify-center my-1 hover:bg-[#3a3a3a] rounded-md m-[2px] transition-all">
+            <Link to={to || "/"} className={`p-1 text-white flex justify-center my-1 hover:bg-[#3a3a3a] rounded-md m-[2px] transition-all ${selected ? "bg-[#3a3a3a]" : "bg-black"}`}>
                 <div className="flex-2 flex">
                     <img className="mr-3" src={icon} alt={alt} />
                     <span className={fix ? "w-[100px] text-center" : ""}>{title}</span>
                 </div>
             </Link>
         ) : (
-            <button onClick={action} className="p-1 flex text-white justify-center my-1 hover:bg-[#3a3a3a] rounded-md m-[2px] transition-all">
+            <button onClick={action} className={`p-1 flex text-white justify-center my-1 hover:bg-[#3a3a3a] rounded-md m-[2px] transition-all ${selected ? "bg-[#3a3a3a]" : "bg-black"}`}>
                 <div className="flex-2 flex">
                     <img className="mr-3" src={icon} alt={alt} />
                     <span className={fix ? "w-[100px] text-center" : ""}>{title}</span>
