325ed93e030e823ac936789ab5ebc4ee80501294
Spyei <caio.salamao1234@gmail.com>
2023-08-15 18:04:29 -0300

    feat: Theme choice, now in dropdown menu

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -2,18 +2,43 @@ import logoutIcon from "../../assets/svgs/logout.svg";
 import plusIcon from "../../assets/svgs/plus.svg";
 import dashIcon from "../../assets/svgs/dashboard.svg";
 import listIcon from "../../assets/svgs/list.svg";
-import React from "react";
+import paletteIcon from "../../assets/svgs/pallete.svg";
+import React, { useState } from "react";
 import api from "../../utils/api";
 import { MenuOption } from "./Option";
+import { ChoiceColor } from "../Colors/Choice";
 
-export const Dropdownmenu: React.FC = () => {
+export const Dropdownmenu: React.FC<{ 
+    setArrowState: (value: boolean) => void 
+}> = ({ setArrowState }) => {
+    const [themeShow, setThemeShow] = useState<boolean>();
+    const selectedTheme = localStorage.getItem("theme") || "blue";
+    const [selected, setSelected] = useState<string>();
+
     return (
-        <div className="flex w-[151px] justify-center items-center">
+        <div className="flex w-[151px] justify-center items-center" onMouseLeave={() => setArrowState(false)}>
             <div className="flex flex-col justify-center">
                 <MenuOption to="/addbot" alt="Plus Icon" icon={plusIcon} title="Adicionar bot" type="link" />
                 <MenuOption to="/" alt="List Icon" icon={listIcon} title="Botlist" type="link" fix />
                 <MenuOption to="/dashboard" alt="Dashboard Icon" icon={dashIcon} title="Dashboard" type="link" fix />
-                <MenuOption alt="Logout Icon" icon={logoutIcon} title="Sair" type="button" action={async () => { await api.logoutUser(); return window.location.reload(); }} fix />
+                <MenuOption alt="Logout Icon" icon={paletteIcon} title="Temas" type="button" action={() => {
+                    setThemeShow(!themeShow);
+                    selected === "theme" ? setSelected("") : setSelected("theme")
+                }} fix selected={selected === "theme"}/>
+                <MenuOption alt="Logout icon" icon={logoutIcon} title="Sair" type="button" action={async () => { await api.logoutUser(); return window.location.reload(); }} fix />
+            </div>
+            <div className={`absolute border-white border-[1px] bg-black w-[120px] right-[150px] xl:hidden text-white transition-all duration-200 top-[120px] ${
+                themeShow
+                ? "opacity-100"
+                : "opacity-0 invisible"
+            }`}>
+                <div className="flex flex-col w-[100%]">
+                    <ChoiceColor name="Vermelho" theme="red" margin="6px" selected={selectedTheme === "red"} />
+                    <ChoiceColor name="Azul" theme="blue" margin="6px" selected={selectedTheme === "blue"} />
+                    <ChoiceColor name="Roxo" theme="purple" margin="6px" selected={selectedTheme === "purple"} />
+                    <ChoiceColor name="Verde" theme="green" margin="6px" selected={selectedTheme === "green"} />
+                    <ChoiceColor name="Preto" theme="black" margin="6px" selected={selectedTheme === "black"} />
+                </div>
             </div>
         </div>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { UserLogin } from "./Login/Discordlogin";
 import { InputSearch } from "./Search/InputSearch";
-import { ColorButton } from "./Colors/ColorButton";
 
 export const Header: React.FC = () => {
     return (
-        <div className="w-[100%] border-b-[1px] border-[#8b8b8b] flex justify-center items-center bg-tansparent to-[100%] xp:flex-wrap">
+        <div className="w-[100vw] relative border-b-[1px] border-[#8b8b8b] flex justify-center items-center bg-tansparent xp:flex-wrap">
             <div className="flex w-[600px] xl:w-[100vw] xl:justify-center">
-                <h1 className="text-white flex flex-row text-[32px] mx-10 my-3">
-                    <strong className="text-[#ffffff]">Bot</strong>
-                    <div className="text-[#a8a8a8]">List</div>
-                </h1>
+                <h1 className="text-white flex flex-row text-[32px] mx-10 my-3"><strong>Simo</strong></h1>
             </div>
             <InputSearch />
-            <ColorButton />
             <UserLogin />
         </div>
     );
