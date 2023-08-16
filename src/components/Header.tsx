309e35cb2d2485b19b6194e65b9b74e5615be6f1
Spyei <caio.salamao1234@gmail.com>
2023-08-16 17:33:19 -0300

    feat: Responsiving footer to all displays

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { InputSearch } from "./Search/InputSearch";
 
 export const Header: React.FC = () => {
     return (
-        <div className="w-[100vw] relative border-b-[1px] border-[#8b8b8b] flex justify-center items-center bg-tansparent xp:flex-wrap">
-            <div className="flex w-[600px] xl:w-[100vw] xl:justify-center">
+        <div className="w-[100vw] border-b-[1px] border-[#8b8b8b] flex justify-center items-center bg-tansparent xl:flex-wrap">
+            <div className="flex w-[600px] xl:w-[100vw] xl:justify-center xl:items-center">
                 <h1 className="text-white flex flex-row text-[32px] mx-10 my-3"><strong>Simo</strong></h1>
             </div>
             <InputSearch />
