befecba5311f7328b6f648b4a52753edbd65f0a4
Spyei <caio.salamao1234@gmail.com>
2023-08-17 17:14:03 -0300

    feat: New patter of theme colors

---
+++ src/utils/theme/border&bg.ts
@@ -0,0 +1,9 @@
+import { ThemeStructure } from '../../types';
+
+export const borderAndBg: ThemeStructure = {
+    blue: "border-blue-400 bg-blue-600",
+    green: "border-green-400 bg-green-600",
+    red: "border-red-400 bg-red-600",
+    purple: "border-purple-400 bg-purple-600",
+    black: "border-white bg-neutral-800"
+};
