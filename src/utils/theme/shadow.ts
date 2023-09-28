fd7987086fd119a4bf1c617e8ae56b042a9ae667
Spyei <caio.salamao1234@gmail.com>
2023-09-28 18:01:41 -0300

    fix: Shadow color

--- src/utils/theme/shadow.ts
+++ src/utils/theme/shadow.ts
@@ -2,7 +2,7 @@ import { ThemeStructure } from '../../types';
 
 export const shadowColor: ThemeStructure = {
     blue: "border-blue-500 shadow-blue-500",
-    green: "border-green-500 shadow-purple-500",
+    green: "border-green-500 shadow-green-500",
     red: "border-red-500 shadow-red-500",
     purple: "border-purple-500 shadow-purple-500",
     black: "border-white shadow-white"
