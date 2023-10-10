fdde23a443a51e01e07e3cb0e39c1a66cc2f642c
Spyei <caio.salamao1234@gmail.com>
2023-10-10 18:38:12 -0300

    feat: Header now is sticked

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
     return (
-        <header className="flex items-center justify-center w-[100vw] max-w-[1500px]">
-            <div className="w-[100%] border-b-[1px] border-[#8b8b8b] flex justify-center items-center bg-tansparent xl:flex-wrap">
-                <Link to="/" className="flex w-[600px] xl:w-[100vw] xl:justify-center xl:items-center">
-                    <h1 className="text-white flex flex-row text-[32px] mx-10 my-3"><strong>Simo</strong></h1>
-                </Link>
-                <InputSearch />
-                <UserLogin />
+        <>
+            <div className="xlr:sticky xlr:top-0 w-[100vw] max-w-[1500px] xlr:z-10 backdrop-blur-sm bg-transparent h-[74px]">
+                <div className="w-[100%] flex justify-center items-center bg-transparent xl:flex-wrap">
+                    <Link to="/" className="flex w-[300px] xl:w-[100vw] xl:justify-center xl:items-center">
+                        <h1 className="text-white flex flex-row text-[32px] mx-10 my-3"><strong>Simo</strong></h1>
+                    </Link>
+                    <InputSearch />
+                    <UserLogin />
+                </div>
             </div>
-        </header>
+            <hr className="w-[100vw] fixed mt-[74px] xl:invisible" />
+        </>
     );
 };
