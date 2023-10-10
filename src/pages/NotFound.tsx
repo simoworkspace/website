12c35c3f3648e065180d3ea8e57fdeb01b619241
Spyei <caio.salamao1234@gmail.com>
2023-10-10 12:56:54 -0300

    feat: Cute (uwu) 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,34 @@
-import catImage from ".././assets/images/404.jpeg";
-import React from 'react';
+import React, { useContext, useEffect, useState } from "react";
+import { buttonColor } from '../utils/theme/button';
+import { ThemeContext } from "../contexts/ThemeContext";
+import { borderColor } from "../utils/theme/border";
+import  axios, { AxiosResponse } from "axios";
 
 export const NotFound: React.FC = () => {
+    const { color } = useContext(ThemeContext);
+    const [cat, setCat] = useState<string>();
+
+    const generateCat = async (): Promise<void> => {
+        const req: AxiosResponse<[{ url: string }]> = await axios.get("https://api.thecatapi.com/v1/images/search");
+        setCat(req.data[0].url);
+    };
+
+    useEffect(() => {
+        generateCat();
+    }, []);
+
     return (
-        <div className="flex justify-center h-[80vh]">
-            <img
-                className="flex w-[min(100%,600px)] object-contain"
-                src={catImage}
-                alt="404 status code cat image UwU"
-            ></img>
-        </div>
+        <section className="w-[100vw] xl:w-[90vw] h-[600px] max-w-[1500px] flex items-center justify-center text-white flex-col">
+            <div className={`border-2 ${borderColor[color]} flex flex-col items-center justify-center text-center break-before-all p-[50px] xl:p-10 rounded-lg bg-neutral-900 gap-4`}>
+                <span className="text-[40px] xl:text-[22px]"><strong>Página não encontrada.</strong></span>
+                <span className="text-[22px] xl:text-[16px]">Nossos anões digitais procuraram por todos os cantos, mas não conseguiram encontrar a página que você está buscando.</span>
+                {cat ? (
+                    <img  className="w-[300px] h-[200px] object-contain" src={cat} />
+                ) : (
+                    <div className="bg-neutral-800 animate-pulse w-[300px] h-[200px]"></div>
+                )}
+                <button onClick={() => location.href = "/"} className={`${buttonColor[color]} transition-colors duration-300 p-3 rounded-lg border-2`}>Take me back</button>
+            </div>
+        </section>
     );
 };
