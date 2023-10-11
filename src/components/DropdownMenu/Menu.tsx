b2c31e56ca0237ac82efe1975462fb8a7ab760be
Spyei <caio.salamao1234@gmail.com>
2023-10-11 10:27:01 -0300

    feat: Adding theme colors to dropdown menu

--- src/components/DropdownMenu/Menu.tsx
+++ src/components/DropdownMenu/Menu.tsx
@@ -2,19 +2,32 @@ import React, { useState, useContext, useRef, useEffect } from "react";
 import * as iconMD from "react-icons/md";
 import * as iconBS from "react-icons/bi";
 import { UserContext } from "../../contexts/UserContext";
-import { Disclosure } from "@headlessui/react";
 import { LoginButton } from "../Login/Login";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { borderColor } from "../../utils/theme/border";
 import { Link } from "react-router-dom";
 import api from "../../utils/api";
+import { Theme, ThemeStructure } from "../../types";
+import { ChoiceColor } from "../Colors/Choice";
 
 export const LoggedMenu: React.FC = () => {
     const { user } = useContext(UserContext);
-    const { color } = useContext(ThemeContext);
+    const { color, changeTheme } = useContext(ThemeContext);
 
     const selectedTheme = localStorage.getItem("theme") || "purple";
     const [themeShow, setThemeShow] = useState<boolean>();
+
+    const themesOptions: ThemeStructure = {
+        black: "bg-[#2e2e2e]",
+        blue: "bg-[#004d7c]",
+        green: "bg-[#04484d]",
+        purple: "bg-[#2B195C]",
+        red: "bg-[#802222]"
+    }
+
+    const toggleTheme = (newTheme: Theme) => {
+        changeTheme(newTheme);
+    };
 
     const [selected, setSelected] = useState<string>();
     const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -27,6 +40,7 @@ export const LoggedMenu: React.FC = () => {
         const handleClickOutside = (event: MouseEvent): void => {
             if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
                 setIsOpen(false);
+                setThemeShow(false);
             }
         }
 
@@ -41,8 +55,10 @@ export const LoggedMenu: React.FC = () => {
         <>
             <section ref={menuRef}>
                 <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    type="button"
+                    onClick={() => {
+                        setIsOpen(!isOpen); 
+                        setThemeShow(false); 
+                    }}
                     className={`${borderColor[color]} xl:invisible bg-neutral-900 flex text-white border-2 p-2 gap-3 rounded-lg max-w-[600px] items-center justify-center`}
                 >
                     <div className="h-[30px] w-[30px]">
@@ -55,42 +71,38 @@ export const LoggedMenu: React.FC = () => {
                         <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`} size={25} />
                     </div>
                 </button>
-                <Disclosure as="div" className={`${isOpen ? "opacity-100" : "opacity-0 invisible"} rounded-t-none border-t-0 text-white p-3 rounded-lg absolute right-1 origin-top-right bg-neutral-900 border-2 transition-all duration-300 ${borderColor[color]}`}>
+                <div className={`${isOpen ? "opacity-100" : "opacity-0 invisible"} rounded-t-none border-t-0 text-white p-3 rounded-lg absolute right-1 origin-top-right bg-neutral-900 border-2 transition-all duration-300 ${borderColor[color]}`}>
                     <Link to="/addbot" className="flex flex-row items-center justify-center text-center gap-3 p-2 rounded-lg transition-colors duration-300 hover:bg-neutral-800 w-[100px]">
-                        <div className="flex justify-start">
+                        <div className="flex w-[100%] items-center justify-start gap-2">
                             <iconBS.BiPlus fill="#fff" size={20} />
-                        </div>
-                        <div>
                             <span>Addbot</span>
                         </div>
                     </Link>
-                    <hr className="my-1" />
                     <button onClick={() => {
                         setThemeShow(!themeShow);
                         selected === "theme" ? setSelected("") : setSelected("theme");
                     }} className="flex flex-row items-center justify-center text-center gap-3 p-2 rounded-lg transition-colors duration-300 hover:bg-neutral-800 w-[100px]">
-                        <div className="flex justify-start">
+                        <div className="flex w-[100%] items-center justify-start gap-2">
                             <iconBS.BiPalette fill="#fff" size={20} />
-                        </div>
-                        <div>
                             <span>Temas</span>
                         </div>
                     </button>
-                    <hr className="my-1" />
                     <button onClick={async () => { await api.logoutUser(); return window.location.reload() }} className="flex flex-row items-center justify-center text-center gap-3 p-2 rounded-lg transition-colors duration-300 hover:bg-neutral-800 w-[100px]">
-                        <div className="flex justify-start w-[30%]">
+                        <div className="flex w-[100%] items-center justify-start gap-2">
                             <iconBS.BiExit fill="#fff" size={20} />
-                        </div>
-                        <div className="flex justify-start w-[70%]">
                             <span>Sair</span>
                         </div>
                     </button>
-                </Disclosure>
+                </div>
             </section>
             <section ref={themeRef}>
-                <Disclosure as="div" className={`${themeShow ? "opacity-100" : "opacity-0 invisible"} text-white p-3 rounded-lg absolute right-1 origin-top-right bg-neutral-900 border-2 transition-all duration-300 ${borderColor[color]}`}>
-                    
-                </Disclosure>
+                <div className={`${themeShow ? "opacity-100" : "opacity-0 invisible"} flex-col flex text-white p-1 rounded-lg absolute right-[131px] top-[120px] origin-top-right bg-neutral-900 border-2 transition-all duration-300 ${borderColor[color]}`}>
+                    <ChoiceColor name="Vermelho" theme="red" margin="6px" selected={selectedTheme === "red"} />
+                    <ChoiceColor name="Azul" theme="blue" margin="6px" selected={selectedTheme === "blue"} />
+                    <ChoiceColor name="Roxo" theme="purple" margin="6px" selected={selectedTheme === "purple"} />
+                    <ChoiceColor name="Verde" theme="green" margin="6px" selected={selectedTheme === "green"} />
+                    <ChoiceColor name="Preto" theme="black" margin="6px" selected={selectedTheme === "black"} />
+                </div>
             </section>
         </>
     ) : (
