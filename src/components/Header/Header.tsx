267e8bde06f3093c85cad1a710054c3b423e030e
Spyei <caio.salamao1234@gmail.com>
2023-10-13 13:37:40 -0300

    fix: mobile header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,17 +7,17 @@ import { NotificationButton } from "../Notification/Button";
 export const Header: React.FC = () => {
     return (
         <>
-            <div className="xlr:sticky xlr:top-0 w-[100vw] max-w-[1500px] xlr:z-10 backdrop-blur-sm bg-transparent h-[74px]">
+            <div className="sticky top-0 w-[100vw] max-w-[1500px] xlr:z-10 backdrop-blur-sm bg-transparent h-[74px] xl:h-[60px]">
                 <div className="w-[100%] flex justify-center items-center bg-transparent xl:flex-wrap">
                     <div className="flex xl:w-[100vw] xl:justify-center xl:items-center">
-                        <Link to="/" className="text-white flex flex-grow flex-row text-[32px] mx-10 my-3"><strong>Simo</strong></Link>
+                        <Link to="/" className="text-white flex flex-grow flex-row text-[32px] mx-10 my-3 xl:mx-0 xl:my-1 xl:justify-center xl:w-full xl:h-full xl:items-center"><strong>Simo</strong></Link>
                     </div>
                     <InputSearch />
                     <NotificationButton/>
                     <UserLogin />
                 </div>
             </div>
-            <hr className="w-[100vw] fixed mt-[74px] xl:invisible" />
+            <hr className="w-[100vw] fixed mt-[74px] xl:mt-[60px]" />
         </>
     );
 };
