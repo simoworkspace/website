03f43f837416fbdba9db11feac6ac80d84a87941
Spyei <caio.salamao1234@gmail.com>
2023-10-13 10:38:26 -0300

    refactor: notification card

--- src/components/Notification/Card.tsx
+++ src/components/Notification/Card.tsx
@@ -23,33 +23,33 @@ export const NotificationCard: FC<{
         setDeleted(false);
     };
 
+    const typeSchemas: {
+        [key: number]: {
+            colors: string,
+            icon: React.ReactNode
+        }
+    } = {
+        0: {
+            colors: "#808080",
+            icon: <icon.BsChat  size={30} fill="#808080"/>
+        },
+        1: {
+            colors: "#03ff4a",
+            icon: <icon.BsCheck2Circle size={30} fill="#03ff4a" />
+        },
+        2: {
+            colors: "#ff3636",
+            icon: <icon.BsXCircle size={30} fill="#ff3636" />
+        }
+    }
+
     return (
-        <div className={`w-[100%] ${borderColor[color]} p-2 flex items-center`}>
-            {notification.type === 0 ? (
-                <div className={`flex items-center gap-3 ${borderColor[color]} border-l-[#808080] border-l-[5px] border-2 rounded-lg p-3 w-full break-words`}>
-                    <icon.BsChat size={30} fill="#808080" />
-                    <span className="w-full">{notification.content}</span>
-                    <button disabled={deleted} onClick={handleDeleteNotification} className="disabled:cursor-default flex h-full justify-start items-center">
-                        {deleted ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={25} className="animate-spin" /> : <icon.BsX size={25} className="hover:fill-red-500 transition-all duration-300" />}
-                    </button>
-                </div>
-            ) : notification.type === 1 ? (
-                <div className={`flex items-center gap-3 ${borderColor[color]} border-l-[#03ff4a] border-l-[5px] border-2 rounded-lg p-3 w-full break-words`}>
-                    <icon.BsCheck2Circle size={30} fill="#03ff4a" />
-                    <span className="w-full">{notification.content}</span>
-                    <button disabled={deleted} onClick={handleDeleteNotification} className="disabled:cursor-default flex h-full justify-start items-center">
-                        {deleted ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={25} className="animate-spin" /> : <icon.BsX size={25} className="hover:fill-red-500 transition-all duration-300" />}
-                    </button>
-                </div>
-            ) : (
-                <div className={`flex items-center gap-3 ${borderColor[color]} border-l-[#ff3636] border-l-[5px] border-2 rounded-lg p-3 w-full break-words`}>
-                    <icon.BsXCircle size={30} fill="#ff3636" />
-                    <span className="w-full">{notification.content}</span>
-                    <button disabled={deleted} onClick={handleDeleteNotification} className="disabled:cursor-default flex h-full justify-start items-center">
-                        {deleted ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={25} className="animate-spin" /> : <icon.BsX size={25} className="hover:fill-red-500 transition-all duration-300" />}
-                    </button>
-                </div>
-            )}
+        <div className={`flex items-center gap-3 ${borderColor[color]} border-l-[${typeSchemas[notification.type].colors}] border-l-[5px] border-2 rounded-lg p-3 w-full break-words`}>
+            {typeSchemas[notification.type].icon}
+            <span className="w-full">{notification.content}</span>
+            <button disabled={deleted} onClick={handleDeleteNotification} className="disabled:cursor-default flex h-full justify-start items-center">
+                {deleted ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={25} className="animate-spin" /> : <icon.BsX size={25} className="hover:fill-red-500 transition-all duration-300" />}
+            </button>
         </div>
     )
 };
