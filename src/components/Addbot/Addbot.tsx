b48495809f5e70ad8ed19faa14cfe3ce26447e8f
Spyei <caio.salamao1234@gmail.com>
2023-10-20 14:45:50 -0300

    feat: checking logged user when he try to addbot

--- src/components/Addbot/Addbot.tsx
+++ src/components/Addbot/Addbot.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { FormAddbot } from "./FormAddbot";
 import { FindBot } from "./FindBot";
 import { FindBotStructure } from "../../types";
 import { FinishAddbot } from "./FinishAddbot";
+import { UserContext } from "../../contexts/UserContext";
 
 export const AddbotComponent: any = () => {
     const [steps, setSteps] = useState<number>(0);
     const [botData, setBotData] = useState<FindBotStructure>();
+    const { user } = useContext(UserContext);
+
+    if (!user) {
+        window.location.href = "/";
+    }
 
     switch (steps) {
         case 0:
