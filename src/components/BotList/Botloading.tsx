efdf4bf00495a725e0264d90d3291260b547792e
Spyei <caio.salamao1234@gmail.com>
2023-10-21 13:09:43 -0300

    feat: grid property to bot loading component

--- src/components/BotList/Botloading.tsx
+++ src/components/BotList/Botloading.tsx
@@ -1,5 +1,17 @@
-export const Botloading: React.FC<{ fills: number }> = ({ fills }) => {
-    return (
+export const Botloading: React.FC<{ fills: number, grid?: boolean }> = ({ fills, grid }) => {
+    return grid ? (
+        Array(fills).fill(
+            <div className="border-2 bg-card-blue rounded-[10px] bg-neutral-950 xl:w-[95vw]">
+                <div className="bg-neutral-900 xl:w-[80px] xl:h-[80px] w-[100px] h-[100px] float-right rounded-full mt-[10px] mr-[10px] self-center animate-pulse"></div>
+                <div className="bg-neutral-900 xl:w-[50%] w-[220px] h-[26px] rounded-[10px] flex ml-20 xl:ml-10 animate-pulse"></div>
+                <div className="bg-neutral-900 xl:w-[65%] w-[70%] h-[72px] mt-3 rounded-[10px] ml-3 animate-pulse"></div>
+                <div className="m-[10px] flex ">
+                    <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
+                    <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
+                </div>
+            </div>
+        )
+    ) : (
         <div className="grid-cols-2 gap-8 m-2 grid xl:grid-cols-1 w-screen max-w-[1500px] xl:flex xl:flex-wrap xl:justify-center">
             {Array(fills).fill(
                 <div className="border-2 bg-card-blue rounded-[10px] bg-neutral-950 xl:w-[95vw]">
@@ -13,5 +25,5 @@ export const Botloading: React.FC<{ fills: number }> = ({ fills }) => {
                 </div>
             )}
         </div>
-    );
+    )
 };
