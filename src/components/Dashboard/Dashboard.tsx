15a4c96da0d40b7a08a5b2f04dc8a8089dc1c972
Spyei <caio.salamao1234@gmail.com>
2023-11-02 19:44:00 -0300

    feat: dashboard panel

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,9 +3,13 @@ import { borderColor } from "../../utils/theme/border";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { UserContext } from "../../contexts/UserContext";
 import * as iconMD from "react-icons/md";
+import * as iconBS from "react-icons/bs";
+import * as iconAI from "react-icons/ai";
 import { BotStructure } from "../../types";
 import { AxiosResponse } from "axios";
 import api from "../../utils/api";
+import { BotCard } from "../BotList/BotCard";
+import { buttonColor } from "../../utils/theme/button";
 
 export const DashboardComponent: React.FC = () => {
     const { color } = useContext(ThemeContext);
@@ -13,6 +17,20 @@ export const DashboardComponent: React.FC = () => {
     const [selectBotMenu, setSelectBotMenu] = useState<boolean>(false);
     const [bots, setBots] = useState<BotStructure[]>();
     const [selectedBot, setSelectedBot] = useState<BotStructure | null>(null);
+    const [apiKey, setApiKey] = useState<string>("");
+    const [apiKeyLoading, setApiKeyLoading] = useState<boolean>(false);
+
+    const handleCreateApiKey = async (botId: string): Promise<void> => {
+        if (apiKey) {
+            await navigator.clipboard.writeText(apiKey);
+            return alert("Copiado apikey para área de transferência.")
+        };
+
+        setApiKeyLoading(true);
+        const req = await api.createApiKey(botId);
+        setApiKey(req.data.api_key);
+        return setApiKeyLoading(false);
+    };
 
     const getUserBots = async (): Promise<void> => {
         const req: AxiosResponse<BotStructure[]> = await api.getAllBots();
@@ -37,7 +55,7 @@ export const DashboardComponent: React.FC = () => {
     return (
         <main className="max-w-[1500px] flex justify-start">
             <section className="w-screen flex flex-row p-5 text-white items-start justify-center gap-10 xl:flex-col">
-                <div className={`${borderColor[color]} border-2 w-[300px] h-[300px] xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
+                <div className={`${borderColor[color]} border-2 w-[300px] xl:h-[400px] h-[300px] xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
                     <div>
                         <img className="rounded-full" src={`https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}.png`} alt={`${user?.username}'s Avatar`} />
                     </div>
@@ -50,11 +68,19 @@ export const DashboardComponent: React.FC = () => {
                     </div>
                 </div>
                 <div className="flex items-center justify-start h-full w-full flex-col">
-                    <h1 className="text-[33px]">Bem vindo a dashboard, <strong>{user?.username}</strong></h1>
+                    <h1 className="text-[33px] text-center">Bem vindo a dashboard, <strong>{user?.username}</strong></h1>
                     <hr className="w-full my-3" />
                     <div className="w-full">
-                        <button onClick={() => setSelectBotMenu(!selectBotMenu)} className={`bg-neutral-900 p-3 items-center justify-center flex flex-row ${borderColor[color]} rounded-lg border-2 w-full h-14`}>
-                            <span className="flex flex-grow">Clique aqui para selecionar um bot para gerenciar.</span>
+                        <button onClick={() => setSelectBotMenu(!selectBotMenu)} className={`bg-neutral-900 p-3 items-center justify-center flex flex-row ${borderColor[color]} rounded-lg border-2 w-full ${selectedBot ? "h-16" : "h-14"}`}>
+                            {selectedBot ? (
+                                <div className="flex items-center justify-start w-full gap-3 p-3">
+                                    <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${selectedBot._id}/${selectedBot.avatar}.png`} />
+                                    <span className="text-xl">{selectedBot.name}</span>
+                                    <span className="text-[#797979] items-center flex text-[13px] xl:invisible justify-center">
+                                        ( {user?.id} )
+                                    </span>
+                                </div>
+                            ) : <span className="flex flex-grow">Clique aqui para selecionar um bot para gerenciar.</span>}
                             <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${selectBotMenu ? "rotate-180" : "rotate-0"}`} size={25} />
                         </button>
                     </div>
@@ -63,7 +89,7 @@ export const DashboardComponent: React.FC = () => {
                             {selectBotMenu && (
                                 <div className={`bg-neutral-900 rounded-b-lg overflow-auto max-h-[300px] w-[95%] ${borderColor[color]} border-2 border-t-0 flex items-center flex-col gap-2 p-3`}>
                                     {bots?.map(bot => (
-                                        <button onClick={() => getSelectedBot(bot._id)} className="flex items-center justify-start w-full gap-3 p-3 transition-colors duration-300 hover:bg-neutral-800 rounded-lg">
+                                        <button onClick={() => getSelectedBot(bot._id)} className="flex xl:flex-col items-center justify-start w-full gap-3 p-3 transition-colors duration-300 hover:bg-neutral-800 rounded-lg">
                                             <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${bot._id}/${bot.avatar}.png`} />
                                             <span className="text-xl">{bot.name}</span>
                                             <span className="text-[#797979] items-center flex text-[13px] justify-center">
@@ -76,8 +102,28 @@ export const DashboardComponent: React.FC = () => {
                         </div>
                     </div>
                     {selectedBot && (
-                        <section>
-                            {selectedBot.name}
+                        <section className={`w-full bg-neutral-900 mt-2 border-2 flex-row ${borderColor[color]} rounded-lg p-4`}>
+                            <div className="grid grid-cols-2 xl:grid-cols-1 gap-8 text-white m-2">
+                                <BotCard bot={selectedBot} />
+                                <div className="w-full flex flex-col gap-3">
+                                    <button className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor["red"]} h-12 transition-colors duration-300 border-2`}>
+                                        <iconBS.BsTrashFill />
+                                        <span>Deletar bot</span>
+                                    </button>
+                                    <button className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor[color]} h-12 transition-colors duration-300 border-2`}>
+                                        <iconMD.MdEditSquare />
+                                        <span>Editar bot</span>
+                                    </button>
+                                    <div className="flex flex-row xl:flex-col bg-neutral-800 w-full h-full rounded-lg items-center">
+                                        <input onAuxClick={async () => {
+                                            await navigator.clipboard.writeText(apiKey);
+                                        }} disabled value={apiKey} placeholder="Clique no botão para solicitar uma api key" className="flex-grow p-2 bg-transparent xl:break-words" />
+                                        <button onClick={() => handleCreateApiKey(selectedBot._id)} className={`${buttonColor[color]} h-full border-2 transition-colors duration-300 rounded-r-lg p-2 xl:w-full xl:rounded-lg flex items-center justify-center`}>
+                                            {apiKeyLoading ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={30} className="animate-spin" /> : apiKey ? <iconMD.MdOutlineContentCopy fill="#fff" size={22} /> : "Gerar"}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </section>
                     )}
                 </div>
