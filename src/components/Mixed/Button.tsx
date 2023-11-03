c1fde5dd664b1253d56e13c8702a43e75586ba27
Spyei <caio.salamao1234@gmail.com>
2023-11-03 18:56:14 -0300

    feat: adding type prop

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -2,10 +2,15 @@ import { useContext } from "react";
 import { buttonColor } from "../../utils/theme/button";
 import { ThemeContext } from "../../contexts/ThemeContext";
 
-export const Button: React.FC<{ action?: () => void; name: string; clas?: string; }> = ({ clas, name, action }) => {
+export const Button: React.FC<{
+    action?: () => void; 
+    clas?: string; 
+    children: React.ReactNode;
+    type?: "button" | "reset" | "submit"
+}> = ({ clas, action, children, type }) => {
     const { color } = useContext(ThemeContext)
     
     return (
-        <button onClick={action} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{name}</button>
+        <button type={type} onClick={action} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</button>
     )
 };
