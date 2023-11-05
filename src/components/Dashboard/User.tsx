881b64e588893caa25410f3590e64d758e5ea1e2
Spyei <caio.salamao1234@gmail.com>
2023-11-05 16:19:56 -0300

    feat: User component in dashb oard

---
+++ src/components/Dashboard/User.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect, useState } from "react";
+import { borderColor } from "../../utils/theme/border";
+import { Theme, UserStructure } from "../../types";
+import { Button } from "../Mixed/Button";
+import * as icon from "react-icons/bs";
+
+export const DashboardUser: FC<{
+    color: Theme;
+    user: UserStructure;
+}> = ({ color, user }) => {
+    const [page, setPage] = useState<boolean>();
+
+    useEffect(() => {
+        setPage(location.href.split("/")[4] === "settings");
+    }, []);
+
+    return (
+        <div className={`${borderColor[color]} border-2 w-[300px] py-8 xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
+            <div>
+                <img className="rounded-full" src={`https://cdn.discordapp.com/avatars/${user?.id}/${user?.avatar}.png`} alt={`${user?.username}'s Avatar`} />
+            </div>
+            <hr className="w-[80%] my-5" />
+            <div className="flex flex-col text-center justify-center">
+                <strong>{user?.username}</strong>
+                <span className="text-[#797979] items-center flex text-[13px] justify-center">
+                    ( {user?.id} )
+                </span>
+            </div>
+            <div className="mt-8">
+                {!page ? <Button clas="text-center flex items-center gap-1" link to={`/dashboard/settings`}><icon.BsGear/>Configurações</Button> : <Button clas="text-center flex items-center gap-1" link to={`/dashboard`}><icon.BsBack/>Voltar</Button>}
+            </div>
+        </div>
+    )
+};
