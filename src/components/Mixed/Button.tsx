b9e6c2ab6ac47e147eaaff7689450bb17e7e0023
Spyei <caio.salamao1234@gmail.com>
2023-11-05 16:20:22 -0300

    fix: action types

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -4,18 +4,19 @@ import { ThemeContext } from "../../contexts/ThemeContext";
 import { Link } from "react-router-dom";
 
 export const Button: React.FC<{
-    action?: () => void; 
+    action?: (() => void) | (() => Promise<void>); 
     clas?: string; 
     children: React.ReactNode;
     type?: "button" | "reset" | "submit"
     link?: boolean;
     to?: string
-}> = ({ clas, action, children, type, link, to }) => {
+    disabled?: boolean;
+}> = ({ clas, action, children, type, link, to, disabled }) => {
     const { color } = useContext(ThemeContext)
     
     return link ? (
         <Link to={to || "/"} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</Link>
     ) : (
-        <button type={type} onClick={action} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</button>
+        <button disabled={disabled} type={type} onClick={action} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</button>
     )
 };
