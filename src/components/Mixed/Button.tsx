249e2a1a1382aef9345770b0057ad3e6cb7ef66d
Spyei <caio.salamao1234@gmail.com>
2023-11-05 14:08:12 -0300

    feat: Link property to Button custom component

--- src/components/Mixed/Button.tsx
+++ src/components/Mixed/Button.tsx
@@ -1,16 +1,21 @@
 import { useContext } from "react";
 import { buttonColor } from "../../utils/theme/button";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { Link } from "react-router-dom";
 
 export const Button: React.FC<{
     action?: () => void; 
     clas?: string; 
     children: React.ReactNode;
     type?: "button" | "reset" | "submit"
-}> = ({ clas, action, children, type }) => {
+    link?: boolean;
+    to?: string
+}> = ({ clas, action, children, type, link, to }) => {
     const { color } = useContext(ThemeContext)
     
-    return (
+    return link ? (
+        <Link to={to || "/"} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</Link>
+    ) : (
         <button type={type} onClick={action} className={`${buttonColor[color]} border-2 transition-all duration-300 w-full text-white p-3 rounded-lg ${clas}`}>{children}</button>
     )
 };
