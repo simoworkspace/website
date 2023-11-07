d1009c2de4bdb4f4d53f92bd2d8bc89011857926
Spyei <caio.salamao1234@gmail.com>
2023-11-07 16:35:51 -0300

    feat: teams component

--- src/components/Dashboard/Settings.tsx
+++ src/components/Dashboard/Settings.tsx
@@ -9,6 +9,7 @@ import * as icon from "react-icons/bs";
 import * as iconAI from "react-icons/ai";
 import api from "../../utils/api";
 import { Link } from "react-router-dom";
+import { Teams } from "../Team/Teams";
 
 export const Settings: FC = () => {
     const { user } = useContext(UserContext);
@@ -62,34 +63,7 @@ export const Settings: FC = () => {
                         </div>
                         <div className="p-3">
                             <span className="text-lg font-bold">Seus times</span>
-                            <div className="flex flex-col gap-2">
-                                <Button disabled={teams?.length === 0} link to="/teams/create" clas="disabled:opacity-50 flex items-center justify-center gap-2 mt-2 w-[190px]"><icon.BsPlusLg size={22} />Criar time</Button>
-                                {teams ? (
-                                    <div className="flex flex-wrap gap-3">
-                                        {teams.map(team => (
-                                            <Link to={`/teams/${team.id}`} className="bg-neutral-800 p-3 rounded-lg duration-300 transition-colors hover:bg-neutral-700">
-                                                <div className="flex flex-row gap-2 items-center justify-center">
-                                                    <img className="w-24 rounded-full" src={team.avatar_url} />
-                                                    <div className="flex flex-col justify-start items-start">
-                                                        <span className="text-lg font-bold">{team.name}</span>
-                                                        <span><strong>{team.members?.length}</strong> Membros</span>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        ))}
-                                    </div>
-                                ) : (
-                                    <div className="bg-neutral-800 p-3 rounded-lg duration-300 h-32 w-64">
-                                        <div className="flex flex-row gap-2 items-center justify-center">
-                                            <div className="w-24 rounded-full h-24 bg-neutral-900 animate-pulse" ></div>
-                                            <div className="flex flex-col justify-start items-start gap-3">
-                                                <div className="w-28 bg-neutral-900 animate-pulse h-5 rounded-full"></div>
-                                                <div className="w-24 bg-neutral-900 animate-pulse h-4 rounded-full"></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
+                            <Teams teams={teams}/>
                         </div>
                     </div>
                 </div>
