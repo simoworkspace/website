9d5d85b7ff0d722587805bcd6e8f269bcae75b33
Spyei <caio.salamao1234@gmail.com>
2023-11-07 14:44:08 -0300

    feat: teams to dashboard

--- src/components/Dashboard/Settings.tsx
+++ src/components/Dashboard/Settings.tsx
@@ -2,20 +2,29 @@ import { FC, useContext, useState, useEffect } from "react";
 import { DashboardUser } from "./User";
 import { UserContext } from "../../contexts/UserContext";
 import { ThemeContext } from "../../contexts/ThemeContext";
-import { UserStructure } from "../../types";
+import { Team, UserStructure } from "../../types";
 import { borderColor } from "../../utils/theme/border";
 import { Button } from "../Mixed/Button";
 import * as icon from "react-icons/bs";
 import * as iconAI from "react-icons/ai";
 import api from "../../utils/api";
+import { Link } from "react-router-dom";
 
 export const Settings: FC = () => {
     const { user } = useContext(UserContext);
     const { color } = useContext(ThemeContext);
     const [bio, setBio] = useState<string>("");
     const [bioPatched, setBioPatched] = useState<boolean>(false);
+    const [bioSubmit, setBioSubmit] = useState<boolean>(false);
+    const [teams, setTeams] = useState<Team[]>();
 
-    const handleBioChange = (event: React.ChangeEvent<HTMLInputElement>): void => setBio(event.target.value); 
+    const handleBioChange = (event: React.ChangeEvent<HTMLInputElement>): void => setBio(event.target.value);
+
+    const getUserTeams = async (): Promise<void> => {
+        const req = await api.getUserTeams();
+
+        setTeams(req.data);
+    };
 
     const patchBio = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
         event.preventDefault();
@@ -23,25 +32,64 @@ export const Settings: FC = () => {
         await api.patchUser(user?.id as string, { bio });
         setBio(bio);
         setBioPatched(false);
+        setBioSubmit(true);
     };
+
+    useEffect(() => {
+        getUserTeams();
+    }, []);
 
     return (
         <main className="max-w-[1500px] flex justify-center items-center">
-            <section className="w-screen flex flex-row p-5 text-white items-start xl:items-center justify-center gap-10 xl:flex-col h-full">
+            <section className="w-screen flex flex-row p-5 text-white items-start xl:items-center justify-center gap-10 xl:flex-col">
                 <DashboardUser color={color} user={user as UserStructure} />
                 <div className="flex h-full w-full flex-col">
                     <h1 className="text-[33px] text-center">Bem vindo a dashboard, <strong>{user?.username}</strong></h1>
                     <hr className="w-full my-3" />
                     <div className={`w-full p-3 h-full flex flex-col gap-3 bg-neutral-900 ${borderColor[color]} border-2 rounded-lg`}>
                         <div className="flex xl:flex-col gap-3 w-full p-3">
-                            <div className="flex flex-col items-center justify-center flex-grow">
+                            <div className="flex flex-col items-start justify-center flex-grow h-14">
                                 <h1 className="font-bold text-lg">Biografia</h1>
                                 <span>Fale mais sobre você, digite ao lado uma breve descrição sobre você</span>
                             </div>
                             <form onSubmit={patchBio} className="w-[50%] xl:w-full xl:h-14 flex flex-row">
-                                <input maxLength={200} value={bio} required disabled={bioPatched} onChange={handleBioChange} className={`bg-transparent disabled:opacity-50 rounded-r-none focus:outline-none border-2 rounded-lg p-2 w-full h-full ${borderColor[color]}`} type="text" />
-                                <Button type="submit" disabled={bioPatched} clas="w-14 flex items-center justify-center rounded-l-none diabled:opacity-50">{bioPatched ? <iconAI.AiOutlineLoading3Quarters size={22} className="animate-spin"/> : <icon.BsCheck size={22} />}</Button>
+                                <div className="w-full">
+                                    <input placeholder="Digite sua biografia aqui." maxLength={200} value={bio} required disabled={bioPatched} onChange={handleBioChange} className={`bg-transparent disabled:opacity-50 rounded-r-none focus:outline-none border-2 rounded-lg p-2 w-full h-14 ${borderColor[color]}`} type="text" />
+                                    {bioSubmit && bio && <span>Bio atualizada com sucesso! <Link className="text-blue-500 hover:underline" to={`/users/${user?.id}`}>Ver perfil</Link></span>}
+                                </div>
+                                <Button type="submit" disabled={bioPatched} clas="w-14 h-14 flex items-center justify-center rounded-l-none diabled:opacity-50">{bioPatched ? <iconAI.AiOutlineLoading3Quarters size={22} className="animate-spin" /> : <icon.BsCheck size={22} />}</Button>
                             </form>
+                        </div>
+                        <div className="p-3">
+                            <span className="text-lg font-bold">Seus times</span>
+                            <div className="flex flex-col gap-2">
+                                <Button disabled={teams?.length === 0} link to="/teams/create" clas="disabled:opacity-50 flex items-center justify-center gap-2 mt-2 w-[190px]"><icon.BsPlusLg size={22} />Criar time</Button>
+                                {teams ? (
+                                    <div className="flex flex-wrap gap-3">
+                                        {teams.map(team => (
+                                            <Link to={`/teams/${team.id}`} className="bg-neutral-800 p-3 rounded-lg duration-300 transition-colors hover:bg-neutral-700">
+                                                <div className="flex flex-row gap-2 items-center justify-center">
+                                                    <img className="w-24 rounded-full" src={team.avatar_url} />
+                                                    <div className="flex flex-col justify-start items-start">
+                                                        <span className="text-lg font-bold">{team.name}</span>
+                                                        <span><strong>{team.members?.length}</strong> Membros</span>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <div className="bg-neutral-800 p-3 rounded-lg duration-300 h-32 w-64">
+                                        <div className="flex flex-row gap-2 items-center justify-center">
+                                            <div className="w-24 rounded-full h-24 bg-neutral-900 animate-pulse" ></div>
+                                            <div className="flex flex-col justify-start items-start gap-3">
+                                                <div className="w-28 bg-neutral-900 animate-pulse h-5 rounded-full"></div>
+                                                <div className="w-24 bg-neutral-900 animate-pulse h-4 rounded-full"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
