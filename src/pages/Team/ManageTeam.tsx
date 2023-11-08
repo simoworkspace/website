f0e2b892b1408d5d22080353d6f151f611d12891
Spyei <caio.salamao1234@gmail.com>
2023-11-08 20:24:09 -0300

    feat: manage team component

---
+++ src/pages/Team/ManageTeam.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { ManageTeamComponent } from "../../components/Team/ManageTeam";
+
+export const ManageTeamPage: FC = () => {
+    return <ManageTeamComponent/>
+};
