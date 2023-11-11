0642885f11d15e235dfac6b5becb6ef3de0eb6ca
Spyei <caio.salamao1234@gmail.com>
2023-11-11 19:05:58 -0300

    chore: implementing new types to team

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -20,36 +20,25 @@ export const TeamComponent: React.FC = () => {
     const [team, setTeam] = useState<Team>();
     const [deleteTeam, setDeleteTeam] = useState<boolean>(false);
     const [teamBot, setTeamBot] = useState<BotStructure | null>(null);
-    const [teamMembers, setTeamMembers] = useState<{ avatar: string; id: string; username: string, owner: boolean }[]>([]);
 
     const { color } = useContext(ThemeContext);
 
     const getTeam = async (): Promise<void> => {
-        const { data: { members, bot_id }, data } = await api.getTeam(teamID);
-
-        const ownersToAdd: { avatar: string; id: string; username: string, owner: boolean }[] = [];
-
-        if (members) {
-            for (const member of members) {
-                const { data: { avatar, id, username } } = await api.getDiscordUser(member.id);
-                ownersToAdd.push({ avatar, id, username, owner: !!members.find(a => a.owner && a.id === member.id) });
-            }
-        }
+        const { data: { bot_id }, data } = await api.getTeam(teamID);
 
         if (bot_id) {
-            const botInfo = (await api.getBotInfos(bot_id)).data;
-            setTeamBot(botInfo);
+            const { data } = await api.getBotInfos(bot_id);
+            setTeamBot(data);
         }
 
         setTeam(data);
-        setTeamMembers([...teamMembers, ...ownersToAdd]);
     };
 
     useEffect(() => {
         getTeam();
     }, [])
 
-    return team && teamMembers ? (
+    return team ? (
         <main className="max-w-[1500px] flex justify-start">
             <section className="w-screen flex flex-row p-5 text-white items-start justify-start gap-10 xl:flex-col">
                 <div className={`${borderColor[color]} border-2 w-[300px] p-5 xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
@@ -68,7 +57,7 @@ export const TeamComponent: React.FC = () => {
                         </span>
                     </div>
                     <div className="flex w-full flex-col gap-3 py-3 px-5">
-                        {teamMembers.find((member) => member.owner && member.id === user?.id) && (
+                        {team.members?.find((member) => member.owner && member.id === user?.id) && (
                             <div className="flex flex-col gap-2">
                                 <Button link to={"/team/manage/" + team.id} clas="w-full flex gap-3 items-center"><icon.BiWrench />Gerenciar</Button>
                                 <button onClick={() => setDeleteTeam(true)} className={`flex items-center flex-row gap-3 p-3 w-full rounded-lg ${buttonColor["red"]} h-12 transition-colors duration-300 border-2`}>
@@ -79,7 +68,7 @@ export const TeamComponent: React.FC = () => {
                         )}
                         <span className="text-lg font-bold text-left">Membros</span>
                         <div className="flex flex-wrap w-full gap-2">
-                            {teamMembers.map((member) => (
+                            {team.members?.map((member) => (
                                 <Link to={`/user/${member.id}`}>
                                     {member.owner && <icon.BiSolidCrown fill="#FFD700" className="absolute ml-7 rotate-45" />}
                                     <img
