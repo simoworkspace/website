eaacdcfc0ceba6454541bf795d06c177f26d7b94
Spyei <caio.salamao1234@gmail.com>
2023-11-11 17:59:42 -0300

    fix: disabled hovers button

--- src/utils/theme/button.ts
+++ src/utils/theme/button.ts
@@ -1,9 +1,9 @@
 import { ThemeStructure } from '../../types';
 
 export const buttonColor: ThemeStructure = {
-    blue: "bg-slate-900 hover:bg-slate-700 border-slate-700",
-    green: "bg-green-900 hover:bg-[#1d733f] border-[#1d733f]",
-    red: "bg-[#4a1110] hover:bg-red-900 border-red-900",
-    purple: "bg-[#301542] hover:bg-purple-900 border-purple-900",
-    black: "bg-neutral-900 border-neutral-500 hover:bg-neutral-500"
+    blue: "bg-slate-900 hover:bg-slate-700 border-slate-700 disabled:hover:bg-slate-900",
+    green: "bg-green-900 hover:bg-[#1d733f] border-[#1d733f] disabled:hover:bg-green-900",
+    red: "bg-[#4a1110] hover:bg-red-900 border-red-900 disabled:hover:bg-[#4a1110]",
+    purple: "bg-[#301542] hover:bg-purple-900 border-purple-900 disabled:hover:bg-[#301542]",
+    black: "bg-neutral-900 border-neutral-500 hover:bg-neutral-500 disabled:hover:bg-neutral-900"
 };
