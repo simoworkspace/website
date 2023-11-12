d54cf8fb01a4d0ffc8223c2f32393637ef20d8ca
Spyei <caio.salamao1234@gmail.com>
2023-11-12 14:34:14 -0300

    feat: manage members component

---
+++ src/components/Team/ManageMembers.tsx
@@ -0,0 +1,220 @@
+import { FC, useState, useEffect, useContext } from "react";
+import { Team, TeamMember, Theme } from "../../types";
+import { borderColor } from "../../utils/theme/border";
+import * as iconMD from "react-icons/md";
+import * as iconBI from "react-icons/bi";
+import * as iconBS from "react-icons/bs";
+import { Button } from "../Mixed/Button";
+import api from "../../utils/api";
+import { Params, useParams } from "react-router-dom";
+import { UserContext } from "../../contexts/UserContext";
+
+export const ManageMembers: FC<{ color: Theme }> = ({ color }) => {
+    const [actions, setActions] = useState<{
+        menu?: boolean;
+        loading?: boolean;
+    }>({ menu: false, loading: false });
+    const params: Params = useParams<string>();
+    const teamID: string = params.teamId as string;
+
+    const [team, setTeam] = useState<Team | null>(null);
+    const [member, setMember] = useState<TeamMember | null>(null);
+    const { menu, loading } = actions;
+    const { user } = useContext(UserContext);
+
+    const getTeam = async (): Promise<void> => {
+        const { data } = await api.getTeam(teamID);
+        
+        setMember(data.members?.find((teamMember) => teamMember.id === member?.id) as TeamMember);
+        setTeam(data);
+    }
+
+    const kickMember = async (): Promise<void> => {
+        if (!team) return;
+
+        setActions({ loading: true });
+
+        const members = team?.members?.filter((teamMember) => teamMember.id !== member?.id);
+
+        await api.patchTeam(team?.id as string, {
+            avatar_url: team.avatar_url,
+            description: team.description,
+            name: team.name,
+            members
+        });
+
+        await getTeam();
+
+        setActions({ loading: false });
+    };
+
+    const demoteMember = async (): Promise<void> => {
+        if (!team || !member || !team.members) return;
+
+        setActions({ loading: true });
+
+        const members = team.members.filter((teamMember) => teamMember.id !== member.id);
+
+        await api.patchTeam(team?.id as string, {
+            avatar_url: team.avatar_url,
+            description: team.description,
+            name: team.name,
+            members: [
+                ...members,
+                {
+                    id: member.id,
+                    username: member.username,
+                    avatar: member.avatar,
+                    permission: 1
+                }]
+        });
+
+        await getTeam();
+
+        setActions({ loading: false });
+    };
+
+    const promoveMember = async (): Promise<void> => {
+        if (!team || !member || !team.members) return;
+
+        setActions({ loading: true });
+
+        const members = team.members.filter((teamMember) => teamMember.id !== member.id);
+
+        await api.patchTeam(team?.id as string, {
+            avatar_url: team.avatar_url,
+            description: team.description,
+            name: team.name,
+            members: [
+                ...members,
+                {
+                    id: member.id,
+                    username: member.username,
+                    avatar: member.avatar,
+                    permission: 0
+                }]
+        });
+
+        await getTeam();
+
+        setActions({ loading: false });
+    };
+
+    const transferPosse = async (): Promise<void> => {
+        if (!team || !member || !team.members) return;
+
+        setActions({ loading: true });
+
+        const members = team.members.filter((teamMember) => teamMember.id !== teamMember?.id && !teamMember.owner);
+        const owner = team.members.find((teamMember) => teamMember.owner);
+
+        if (!owner) return;
+
+        await api.patchTeam(team?.id as string, {
+            avatar_url: team.avatar_url,
+            description: team.description,
+            name: team.name,
+            members: [
+                ...members,
+                {
+                    id: owner.id,
+                    username: owner.username,
+                    avatar: owner.avatar,
+                    permission: 0
+                },
+                {
+                    id: member.id,
+                    username: member.username,
+                    avatar: member.avatar,
+                    owner: true
+                }]
+        });
+
+        await getTeam();
+
+        setActions({ loading: false });
+    };
+
+    useEffect(() => {
+        getTeam();
+    }, []);
+
+    const youinTeam = team?.members?.find((member) => member.id === user?.id);
+
+    return (
+        <section className="flex w-full items-center justify-center flex-col gap-2">
+            <div className="flex flex-col w-full">
+                <div>
+                    <button onClick={() => {
+                        setActions({ menu: !actions.menu });
+                        setMember(null);
+                    }} className={`bg-neutral-900 p-3 items-center justify-center flex flex-row ${borderColor[color]} rounded-lg border-2 w-full`}>
+                        {member ? (
+                            <>
+                                <div className="flex items-center justify-start gap-2 w-full flex-grow">
+                                    <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
+                                    <span className="text-lg font-bold">{member.username}</span>
+                                    <span className="text-neutral-500 xl:invisible">({member.id})</span>
+                                </div>
+                                <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${menu ? "rotate-180" : "rotate-0"}`} size={25} />
+                            </>
+                        ) : (
+                            <>
+                                <span className="flex flex-grow">Clique aqui para selecionar um membro para gerenciar.</span>
+                                <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${menu ? "rotate-180" : "rotate-0"}`} size={25} />
+                            </>
+                        )}
+                    </button>
+                </div>
+                <div className="relative w-full">
+                    <div className={`${menu ? "opacity-100 visible" : "opacity-0 invisible"} transition-all duration-300 w-full flex items-center justify-center`}>
+                        {menu && (
+                            <div className={`bg-neutral-900 rounded-b-lg overflow-auto max-h-[300px] w-[95%] ${borderColor[color]} border-2 border-t-0 flex items-center flex-col gap-2 p-3`}>
+                                {team?.members?.map((member, index) => (
+                                    <button key={index} onClick={() => {
+                                        setMember(member);
+                                        setActions({ menu: false });
+                                    }} className="flex xl:flex-col items-center justify-start w-full gap-3 p-3 transition-colors duration-300 hover:bg-neutral-800 rounded-lg">
+                                        <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
+                                        <span className="text-xl">{member.username}</span>
+                                        <span className="text-[#797979] items-center flex text-[13px] justify-center">
+                                            ( {member.id} )
+                                        </span>
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+            {member && (
+                <div className="p-3 w-full bg-neutral-800 rounded-lg">
+                    <div className="flex flex-row xl xl:flex-col xl:justify-center xl:w-full">
+                        <div className="flex items-center xl:flex-col justify-start gap-3 w-full flex-grow">
+                            <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
+                            <div className="flex flex-col gap-1 xl:text-center">
+                                <span className="text-lg font-bold">{member.username}</span>
+                                <span>Cargo <strong>{member?.permission === 1 ? "Membro" : member.owner ? "Dono" : "Administrador"}</strong></span>
+                            </div>
+                        </div>
+                        <div className="flex flex-row xl:flex-col xl:justify-center xl:w-full gap-2 w-full p-2 justify-end">
+                            {youinTeam?.id !== member.id && (
+                                (youinTeam?.owner || youinTeam?.permission === 0 || member.owner) && (
+                                    <>
+                                        <Button action={kickMember} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Expulsar</span><iconBS.BsHammer /></Button>
+                                        {member?.permission === 1 ? (
+                                            <Button action={promoveMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Promover</span><iconBS.BsArrowUp /></Button>
+                                        ) : (
+                                            <Button action={demoteMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span  className="xl:flex xl:flex-grow xl:justify-center">Rebaixar</span><iconBS.BsArrowDown /></Button>
+                                        )}
+                                        {youinTeam?.id !== member.id && youinTeam?.owner && <Button action={transferPosse} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Transferir posse</span><iconBI.BiSolidCrown /></Button>}
+                                    </>
+                                )
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
+        </section>
+    )
+};
--- src/components/Team/ManageTeam.tsx
+++ src/components/Team/ManageTeam.tsx
@@ -12,6 +12,7 @@ import api from "../../utils/api";
 import { Params, useParams } from "react-router-dom";
 import { PopUpError } from "../Mixed/Error";
 import { ApiErrors } from "../../utils/api/errors";
+import { ManageMembers } from "./ManageMembers";
 
 export const ManageTeamComponent: FC = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<Team>();
@@ -38,7 +39,7 @@ export const ManageTeamComponent: FC = () => {
             await api.patchTeam(team?.id as string, formData);
 
             window.location.href = `/team/${team?.id}`;
-            
+
             setError({
                 show: false
             });
@@ -74,31 +75,34 @@ export const ManageTeamComponent: FC = () => {
                     <div className="w-full">
                     </div>
                     <section className={`w-full bg-neutral-900 mt-2 border-2 flex-row ${borderColor[color]} rounded-lg p-4 items-center justify-center`}>
-                        <div className="text-white xl:text-[26px] text-[40px] m-2 xl:m-0 xl:mt-2 w-full flex items-center justify-center">
-                            <span className="text-white flex flex-row text-[26px] mx-10 my-3">
-                                <h1 className="text-[#ffffff] xl:text-[28px] xl:mr-0 mr-2 font-bold">Editar time <strong>{team?.name}</strong></h1>
-                            </span>
+                        <ManageMembers color={color} />
+                        <div>
+                            <div className="text-white xl:text-[26px] text-[40px] m-2 xl:m-0 xl:mt-2 w-full flex items-center justify-center">
+                                <span className="text-white flex flex-row text-[26px] mx-10 my-3">
+                                    <h1 className="text-[#ffffff] xl:text-[28px] xl:mr-0 mr-2 font-bold xl:text-center">Editar time <strong>{team?.name}</strong></h1>
+                                </span>
+                            </div>
+                            {team ? (
+                                <form onSubmit={handleSubmit(onSubmitEdit)} className="gap-5 items-center justify-center pt-1 flex flex-col">
+                                    <div className="flex flex-col gap-3">
+                                        <Input errors={errors} optional name="name" defaultValue={team?.name} register={register} text="Digite o nome que o seu time irá receber" title="Nome" type="input" maxLength={15} minLength={3} placeholder="Mango Team" />
+                                        <Input errors={errors} optional name="avatar_url" defaultValue={team?.avatar_url} register={register} text="Coloque o link de imagem do avatar do seu time" title="Avatar em URL" inputType="url" type="input" placeholder="https://i.imgur.com/1DBO2wh.jpeg" />
+                                        <Input errors={errors} name="description" register={register} defaultValue={team?.description} text="Digite uma breve descrição sobre seu time" title="Descrição" optional type="input" placeholder="Meu time é um time legal e bonito..." maxLength={50} minLength={5} />
+                                    </div>
+                                    <div className="flex justify-center m-4 xl:m-0 xl:w-full xl:mb-4 items-center gap-3">
+                                        <input
+                                            type="submit"
+                                            value="Salvar alterações"
+                                            disabled={submitedEdit}
+                                            className={`disabled:cursor-default disabled:opacity-70 cursor-pointer transition-all duration-300 items-center border-2 w-[300px] rounded-xl h-[60px] text-white xl:w-full ${buttonColor[color]}`}
+                                        />
+                                        {submitedEdit && <icon.AiOutlineLoading3Quarters fill="#fff" size={30} className="animate-spin" />}
+                                    </div>
+                                </form>
+                            ) : (
+                                <div>carregando...</div>
+                            )}
                         </div>
-                        {team ? (
-                            <form onSubmit={handleSubmit(onSubmitEdit)} className="gap-5 items-center justify-center pt-1 flex flex-col">
-                                <div className="flex flex-col gap-3">
-                                    <Input errors={errors} optional name="name" defaultValue={team?.name} register={register} text="Digite o nome que o seu time irá receber" title="Nome" type="input" maxLength={15} minLength={3} placeholder="Mango Team" />
-                                    <Input errors={errors} optional name="avatar_url" defaultValue={team?.avatar_url} register={register} text="Coloque o link de imagem do avatar do seu time" title="Avatar em URL" inputType="url" type="input" placeholder="https://i.imgur.com/1DBO2wh.jpeg" />
-                                    <Input errors={errors} name="description" register={register} defaultValue={team?.description} text="Digite uma breve descrição sobre seu time" title="Descrição" optional type="input" placeholder="Meu time é um time legal e bonito..." maxLength={50} minLength={5} />
-                                </div>
-                                <div className="flex justify-center m-4 xl:m-0 xl:w-full xl:mb-4 items-center gap-3">
-                                    <input
-                                        type="submit"
-                                        value="Salvar alterações"
-                                        disabled={submitedEdit}
-                                        className={`disabled:cursor-default disabled:opacity-70 cursor-pointer transition-all duration-300 items-center border-2 w-[300px] rounded-xl h-[60px] text-white xl:w-full ${buttonColor[color]}`}
-                                    />
-                                    {submitedEdit && <icon.AiOutlineLoading3Quarters fill="#fff" size={30} className="animate-spin" />}
-                                </div>
-                            </form>
-                        ) : (
-                            <div>carregando...</div>
-                        )}
                     </section>
                 </div>
             </section>
