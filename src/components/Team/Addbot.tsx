2643b424b9a37b306f57d4825c6906dd31a3dc50
Spyei <caio.salamao1234@gmail.com>
2023-11-27 10:57:21 -0300

    feat: team addbot page

---
+++ src/components/Team/Addbot.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Button } from "../Mixed/Button";
+
+export const TeamAddbot: FC = () => {
+    return (
+        <section>
+            oi  
+        </section>
+    )
+};
---
+++ src/pages/Team/Addbot.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { TeamAddbot } from "../../components/Team/Addbot";
+
+export const TeamAddbotPage: FC = () => {
+    return <TeamAddbot/>
+};
