febc4804e841af62afa4406632f26a7a4a7db975
Spyei <caio.salamao1234@gmail.com>
2023-11-27 13:00:17 -0300

    fix: user id in some parts of dashboard

--- src/components/Dashboard/User.tsx
+++ src/components/Dashboard/User.tsx
@@ -20,13 +20,13 @@ export const DashboardUser: FC<{
     return (
         <div className={`${borderColor[color]} border-2 w-[300px] py-8 xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
             <div>
-                <img className="rounded-full" src={`https://cdn.discordapp.com/avatars/${user?._id}/${user?.avatar}.png`} alt={`${user?.username}'s Avatar`} />
+                <img className="rounded-full" src={`https://cdn.discordapp.com/avatars/${user?._id || user.id}/${user?.avatar}.png`} alt={`${user?.username}'s Avatar`} />
             </div>
             <hr className="w-[80%] my-5" />
             <div className="flex flex-col text-center justify-center">
                 <strong>{user?.username}</strong>
                 <span className="text-[#797979] items-center flex text-[13px] justify-center">
-                    ( {user?._id} )
+                    ( {user?._id || user.id} )
                 </span>
             </div>
             <div className="mt-8 w-10/12">
