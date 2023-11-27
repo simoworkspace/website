b04410f23af0f6bf190b060973c0c475df42ba27
Spyei <caio.salamao1234@gmail.com>
2023-11-27 11:42:27 -0300

    chore: bot card button colors

--- src/components/BotList/BotCard.tsx
+++ src/components/BotList/BotCard.tsx
@@ -8,12 +8,13 @@ import { borderColor } from "../../utils/theme/border";
 import * as icon from "react-icons/bs";
 import { Button } from "../Mixed/Button";
 import { borderAndBg } from "../../utils/theme/border&bg";
+import { buttonColor } from "../../utils/theme/button";
 
 export const BotCard: React.FC<{ bot: BotStructure }> = ({ bot }) => {
     const { color } = useContext(ThemeContext);
 
     return (
-        <div className="bg-neutral-900 w-full border-2 border-neutral-800 rounded-lg p-3 transition-colors hover:bg-[#1d1d1d] flex flex-col gap-3 xl:w-[95vw]" key={bot._id}>
+        <div className={`bg-neutral-900 w-full rounded-lg p-3 transition-colors hover:bg-[#1d1d1d] flex flex-col gap-3 xl:w-[95vw] `} key={bot._id}>
             <Link to={`/bot/${bot._id}`} className="flex flex-col gap-3">
                 <div className="flex gap-2 items-center">
                     {!bot.approved && (
@@ -24,31 +25,32 @@ export const BotCard: React.FC<{ bot: BotStructure }> = ({ bot }) => {
                     <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${bot._id}/${bot.avatar}.png`} onError={({ currentTarget }) => { currentTarget.onerror = null; currentTarget.src = simo }} />
                     <div className="flex gap-2 flex-col">
                         <span className="font-bold text-lg">{bot.name}</span>
-                        <div className="flex gap-1">
-                            <TiArrowSortedUp size={22} />
-                            <span className="text-sm">{bot.total_votes}</span>                            
+                        <div className="flex">
+                            <TiArrowSortedUp size={20} />
+                            <span className="text-sm">{bot.total_votes}</span>
                         </div>
                     </div>
                 </div>
                 <div className="flex gap-3 flex-col">
                     <span className="min-h-[48px]">{bot.short_description}</span>
-                    <div className="flex flex-row gap-3 flex-wrap">
+                    <div className="flex flex-row gap-1 flex-wrap">
                         {bot.tags.map(tag => (
                             <div className={`${borderAndBg[color]} p-[6px] rounded-lg border-2`}>{tag}</div>
                         ))}
                     </div>
                 </div>
             </Link>
-            <div className="flex gap-3 p-y2">
-                <Button clas="w-full p-1">Adicionar</Button>
-                <Button link to={`/vote/${bot._id}`} clas="px-5 flex gap-2 items-center justify-center w-[25%] xl:w-full">
-                    <span>Votar</span>
-                    <TiArrowSortedUp size={22} />
-                </Button>
+            <div className="flex gap-3 w-full">
+                <div className="flex w-full flex-grow">
+                    <Link to={bot.invite_url} className={`${buttonColor[color]} text-center border-2 transition-all duration-300 text-white p-3 rounded-lg disabled:opacity-50 disabled:cursor-default w-full bg-neutral-900`}>Adicionar</Link>
+                </div>
+                <div>
+                    <Link to={`/vote/${bot._id}`} className={`${buttonColor[color]} text-center border-2 transition-all duration-300 text-white p-3 rounded-lg disabled:opacity-50 disabled:cursor-default px-5 flex gap-2 items-center justify-center w-full bg-neutral-900`}>
+                        <span>Votar</span>
+                        <TiArrowSortedUp size={22} />
+                    </Link>
+                </div>
             </div>
         </div>
     );
 }
-/*
-src={`https://cdn.discordapp.com/avatars/${bot._id}/${bot.avatar}.png`}
-*/
