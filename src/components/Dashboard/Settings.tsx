2374023df192e48334689adf669ea7f585bbcbda
Spyei <caio.salamao1234@gmail.com>
2023-11-30 20:03:55 -0300

    feat: you can change your banner

--- src/components/Dashboard/Settings.tsx
+++ src/components/Dashboard/Settings.tsx
@@ -16,14 +16,23 @@ export const Settings: FC = () => {
     const { color } = useContext(ThemeContext);
     const [user, setUser] = useState<DBUser>();
     const websiteUser = useContext(UserContext);
-    const [bio, setBio] = useState<string>(user?.bio as string);
-    const [bioPatched, setBioPatched] = useState<boolean>(false);
-    const [bioSubmit, setBioSubmit] = useState<boolean>(false);
+
+    const [editActions, setEditActions] = useState<{ submitedBio?: boolean; submitedBanner?: boolean; bio?: string; banner_url?: string; patchedbio?: boolean; patchedbanner?: boolean }>({
+        banner_url: user?.banner_url,
+        bio: user?.bio,
+        submitedBio: false,
+        submitedBanner: false,
+        patchedbio: false,
+        patchedbanner: false
+    });
+
     const [teams, setTeams] = useState<Team[]>();
 
-    const handleBioChange = (event: React.ChangeEvent<HTMLInputElement>): void => setBio(event.target.value);
+    const handleBioChange = (event: React.ChangeEvent<HTMLInputElement>) => setEditActions({ bio: event?.target.value });
 
-    const getUserTeams = async (): Promise<void> => {
+    const handleBannerChange = (event: React.ChangeEvent<HTMLInputElement>) => setEditActions({ banner_url: event?.target.value });
+
+    const getUserTeams = async () => {
         const req = await api.getUserTeams();
         const req2 = await api.getUserFromDB(websiteUser.user?.id || websiteUser.user?._id as string);
 
@@ -31,13 +40,26 @@ export const Settings: FC = () => {
         setTeams(req.data);
     };
 
-    const patchBio = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    const patchBio = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setBioPatched(true);
-        await api.patchUser(user?._id as string, { bio });
-        setBio(bio);
-        setBioPatched(false);
-        setBioSubmit(true);
+        if (!editActions.bio) {
+            setEditActions({ patchedbio: false });
+        } else {
+            setEditActions({ patchedbio: true });
+            await api.patchUser({ bio: editActions.bio as string });
+            setEditActions({ bio: editActions.bio, patchedbio: false, submitedBio: true });
+        }
+    };
+
+    const patchBanner = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (!editActions.banner_url) {
+            setEditActions({ patchedbanner: false });
+        } else {
+            setEditActions({ patchedbanner: true });
+            await api.patchUser({ banner_url: editActions.banner_url as string });
+            setEditActions({ banner_url: editActions.banner_url, patchedbanner: false, submitedBanner: true });
+        }
     };
 
     useEffect(() => {
@@ -59,10 +81,23 @@ export const Settings: FC = () => {
                             </div>
                             <form onSubmit={patchBio} className="w-[50%] xl:w-full xl:h-14 flex flex-row">
                                 <div className="w-full">
-                                    <input defaultValue={user.bio} placeholder="Digite sua biografia aqui." maxLength={200} value={bio} required disabled={bioPatched} onChange={handleBioChange} className={`bg-transparent disabled:opacity-50 rounded-r-none focus:outline-none border-2 rounded-lg p-2 w-full h-14 ${borderColor[color]}`} type="text" />
-                                    {bioSubmit && bio && <span>Bio atualizada com sucesso! <Link className="text-blue-500 hover:underline" to={`/user/${user?._id}`}>Ver perfil</Link></span>}
+                                    <input defaultValue={user.bio} placeholder="Digite sua biografia aqui." maxLength={200} value={editActions.bio} required disabled={editActions.patchedbio} onChange={handleBioChange} className={`bg-transparent disabled:opacity-50 rounded-r-none focus:outline-none border-2 rounded-lg p-2 w-full h-14 ${borderColor[color]}`} type="text" />
+                                    {editActions.submitedBio && <span>Bio atualizada com sucesso! <Link className="text-blue-500 hover:underline" to={`/user/${user?._id}`}>Ver perfil</Link></span>}
                                 </div>
-                                <Button type="submit" disabled={bioPatched} clas="w-14 h-14 flex items-center justify-center rounded-l-none diabled:opacity-50">{bioPatched ? <iconAI.AiOutlineLoading3Quarters size={22} className="animate-spin" /> : <icon.BsCheck size={22} />}</Button>
+                                <Button type="submit" disabled={editActions.patchedbio} clas="w-14 h-14 flex items-center justify-center rounded-l-none diabled:opacity-50">{editActions.patchedbio ? <iconAI.AiOutlineLoading3Quarters size={22} className="animate-spin" /> : <icon.BsCheck size={22} />}</Button>
+                            </form>
+                        </div>
+                        <div className="flex xl:flex-col gap-3 w-full p-3">
+                            <div className="flex flex-col items-start justify-center flex-grow h-14">
+                                <h1 className="font-bold text-lg">Banner</h1>
+                                <span>Coloque o link de uma imagem para ser seu banner.</span>
+                            </div>
+                            <form onSubmit={patchBanner} className="w-[50%] xl:w-full xl:h-14 flex flex-row">
+                                <div className="w-full">
+                                    <input defaultValue={user?.banner_url} placeholder="Coloque uma URL de uma imagem aqui." maxLength={200} value={editActions.banner_url} required disabled={editActions.patchedbanner} onChange={handleBannerChange} className={`bg-transparent disabled:opacity-50 rounded-r-none focus:outline-none border-2 rounded-lg p-2 w-full h-14 ${borderColor[color]}`} type="text" />
+                                    {editActions.submitedBanner && <span>Banner atualizado com sucesso! <Link className="text-blue-500 hover:underline" to={`/user/${user?._id}`}>Ver perfil</Link></span>}
+                                </div>
+                                <Button type="submit" disabled={editActions.patchedbanner} clas="w-14 h-14 flex items-center justify-center rounded-l-none diabled:opacity-50">{editActions.patchedbanner ? <iconAI.AiOutlineLoading3Quarters size={22} className="animate-spin" /> : <icon.BsCheck size={22} />}</Button>
                             </form>
                         </div>
                         <div className="p-3">
@@ -74,6 +109,6 @@ export const Settings: FC = () => {
             </section>
         </main>
     ) : (
-        <UserLoading/>
+        <UserLoading />
     )
 };
