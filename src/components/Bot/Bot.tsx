d369c5fe05442765b8fdb547c3ce880e77cd4eaf
Spyei <caio.salamao1234@gmail.com>
2023-12-04 16:07:03 -0300

    chore: poor code

--- src/components/Bot/Bot.tsx
+++ src/components/Bot/Bot.tsx
@@ -1,7 +1,6 @@
-import { AxiosResponse } from "axios";
-import React, { useEffect, useState, useContext } from "react";
-import { NavigateFunction, useNavigate, useParams, Link, Params } from "react-router-dom";
-import { BotStructure, DiscordUser, UserStructure } from "../../types";
+import { useEffect, useState, useContext, FC } from "react";
+import { useParams, Link, Params } from "react-router-dom";
+import { BotStructure } from "../../types";
 import api from '../../utils/api';
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { Feedbacks } from "../../components/Feedbacks/Feedbacks";
@@ -12,18 +11,16 @@ import * as icon from "react-icons/bs";
 import { BotLoading } from "./BotLoading";
 import { Helmet } from "react-helmet";
 
-export const BotComponent: React.FC = () => {
+export const BotComponent: FC = () => {
     const params: Params = useParams<string>();
     const botid: string = params.botid as string;
-    const navigate: NavigateFunction = useNavigate();
     const { color } = useContext(ThemeContext);
 
-    const [botData, setBotData] = useState<DiscordUser>();
-    const [bot, setBot] = useState<BotStructure>();
+    const [botData, setBotData] = useState<BotStructure>();
     const [stars, setStars] = useState<number>(0);
     const [dev, setDev] = useState<{ id: string, avatar: string, username: string }>();
 
-    const getBotStars = async (): Promise<void> => {
+    const getBotStars = async () => {
         const res = await api.getBotFeedbacks(params.botid as string);
         const stars = res.data.map(a => a.stars);
         let count = 0;
@@ -31,42 +28,27 @@ export const BotComponent: React.FC = () => {
         return setStars(Math.round(count / stars.length));
     };
 
-    const getBotData = async (): Promise<void> => {
-        const res: AxiosResponse<DiscordUser> = await api.getDiscordUser(botid);
-        return setBotData(res.data);
-    };
+    const getBotData = async () => {
+        const { data: { owner_id }, data } = await api.getBotInfos(botid);
 
-    const verifyBot = async (): Promise<void> => {
-        const res: BotStructure | undefined = (await api.getAllBots()).data.find((bot: BotStructure) => bot._id === botid);
-        if (!res) { navigate("/") };
-    };
-
-    const getBotInDB = async (): Promise<void> => {
-        const res1: AxiosResponse<BotStructure> = await api.getBotInfos(botid);
-        const { owner_id } = res1.data;
-
-        const res: AxiosResponse<DiscordUser> = await api.getDiscordUser(owner_id);
-        const { username, avatar, id } = res.data;
+        const { data: { username, avatar, id } } = await api.getDiscordUser(owner_id);
 
         setDev({ username, avatar, id, });
-
-        return setBot(res1.data);
+        setBotData(data);
     };
 
     useEffect(() => {
-        verifyBot();
         getBotData();
-        getBotInDB();
         getBotStars();
     }, []);
 
-    return botData && bot ? (
+    return botData ? (
         <section className="max-w-[1500px] w-screen">
-            {!bot.approved && (
+            {!botData.approved && (
                 <div className="fixed flex items-center justify-center backdrop-blur-sm inset-0">
                     <div className="flex gap-3 items-center justify-center flex-col w-full h-[150px] border-2 rounded-lg bg-[#e8a60c] border-[#9e7514]">
                         <icon.BsClockFill size={35} />
-                        <span className="text-lg text-center px-2">Bot {bot.name} está em análise, aguarde até que ela seja finalizada.</span>
+                        <span className="text-lg text-center px-2">Bot {botData.name} está em análise, aguarde até que ela seja finalizada.</span>
                     </div>
                 </div>
             )}
@@ -75,14 +57,14 @@ export const BotComponent: React.FC = () => {
                     <div className={`bg-neutral-900 rounded-xl flex xl:flex-col xl:h-[320px] h-[120px] w-[95%] border-2 ${borderColor[color]} items-center justify-center`}>
                         <img
                             className="w-[100px] h-[100px] xl:my-2 rounded-full xl:float-none ml-2"
-                            src={`https://cdn.discordapp.com/avatars/${botData.id}/${botData.avatar}.png`}
-                            alt={botData.username + "'s Avatar"}
+                            src={`https://cdn.discordapp.com/avatars/${botData._id}/${botData.avatar}.png`}
+                            alt={botData.name + "'s Avatar"}
                         />
                         <div className="flex flex-col w-full justify-center gap-2">
                             <div className="ml-6 xl:m-0 xl:my-1 text-white flex xl:flex-col xl:items-center flex-row gap-3 text-[26px]">
-                                <strong>{botData.username}</strong>
+                                <strong>{botData.name}</strong>
                                 <span className="text-[#797979] items-center flex text-[13px]">
-                                    ( {botData.id} )
+                                    ( {botData._id} )
                                 </span>
                             </div>
                             <div className="flex mx-6 xl:justify-center xl:m-1 flex-row gap-1">
@@ -98,13 +80,13 @@ export const BotComponent: React.FC = () => {
                             <div className="flex gap-4 items-center justify-center xl:w-screen flex-row m-4">
                                 <Link
                                     className="border-2 border-neutral-700 bg-neutral-900 text-white hover:bg-neutral-700 transition-colors duration-300 p-2 rounded-md w-[120px] text-center"
-                                    to={`/vote/${botData.id}`}
+                                    to={`/vote/${botData._id}`}
                                 >
                                     <span>Votar</span>
                                 </Link>
                                 <Link
                                     className="border-2 border-neutral-700 bg-neutral-900 text-white hover:bg-neutral-700 transition-colors duration-300 p-2 rounded-md w-[120px] text-center"
-                                    to={`https://discord.com/api/oauth2/authorize?client_id=${botData.id}&permissions=70368744177655&scope=bot%20applications.commands`}
+                                    to={`https://discord.com/api/oauth2/authorize?client_id=${botData._id}&permissions=70368744177655&scope=bot%20applications.commands`}
                                 >
                                     <span>Adicionar</span>
                                 </Link>
@@ -115,7 +97,7 @@ export const BotComponent: React.FC = () => {
                 <section className={`w-[90%] mb-5 bg-neutral-900 border-2 ${borderColor[color]} border-t-0 rounded-t-none rounded-lg p-10 xl:p-3`}>
                     <div className="flex flex-row xl:flex-col">
                         <div className="w-[80%] xl:w-full flex break-words xl:justify-center p-2">
-                            <Markdown markdown={bot.long_description} />
+                            <Markdown markdown={botData.long_description} />
                         </div>
                         <div className="w-[1px] bg-[#8b8b8b]" />
                         <hr className="xl:my-4 xl:w-full" />
@@ -137,15 +119,15 @@ export const BotComponent: React.FC = () => {
                                 <hr className="my-4 w-full" />
                                 <div className="flex flex-col w-full gap-3">
                                     <div>
-                                        <strong className="text-lg">Prefixo </strong><span>{bot.prefixes.join(", ")}</span>
+                                        <strong className="text-lg">Prefixo </strong><span>{botData.prefixes.join(", ")}</span>
                                     </div>
                                     <div>
-                                        <strong className="text-lg">Votos </strong><span>{bot.total_votes}</span>
+                                        <strong className="text-lg">Votos </strong><span>{botData.total_votes}</span>
                                     </div>
                                     <div>
                                         <div className="flex flex-row gap-3 flex-wrap">
                                             <strong className="text-lg">Tags</strong>
-                                            {bot.tags.map(tag => (
+                                            {botData.tags.map(tag => (
                                                 <div className={`${borderAndBg[color]} p-[6px] rounded-lg border-2`}>{tag}</div>
                                             ))}
                                         </div>
@@ -153,22 +135,22 @@ export const BotComponent: React.FC = () => {
                                     <h1 className="text-2xl text-center">Links</h1>
                                     <hr className="my-1 w-full" />
                                     <div className="flex flex-col gap-3 flex-wrap">
-                                        {bot?.support_server && (
-                                            <Link to={bot?.support_server.includes("https://") ? bot?.support_server : "https://" + bot?.support_server} className="flex items-center gap-3 p-2">
+                                        {botData?.support_server && (
+                                            <Link to={botData?.support_server.includes("https://") ? botData?.support_server : "https://" + botData?.support_server} className="flex items-center gap-3 p-2">
                                                 <icon.BsDiscord size={30} fill="#5662F6" />
                                                 <span>Servidor de suporte</span>
                                             </Link>
                                         )}
-                                        {bot?.source_code && (
-                                            <Link to={bot?.source_code.includes("https://") ? bot?.source_code : "https://" + bot?.source_code} className="flex items-center gap-3 p-2">
+                                        {botData?.source_code && (
+                                            <Link to={botData?.source_code.includes("https://") ? botData?.source_code : "https://" + botData?.source_code} className="flex items-center gap-3 p-2">
                                                 <icon.BsGithub size={30} />
                                                 <span>Repositório</span>
                                             </Link>
                                         )}
-                                        {bot?.website_url && (
-                                            <Link to={bot?.website_url.includes("https://") ? bot?.website_url : "https://" + bot?.website_url} className="flex items-center gap-3 p-2">
+                                        {botData?.website_url && (
+                                            <Link to={botData?.website_url.includes("https://") ? botData?.website_url : "https://" + botData?.website_url} className="flex items-center gap-3 p-2">
                                                 <icon.BsGlobe size={30} />
-                                                <span>Website (<span className="text-blue-600">{bot?.website_url}</span>)</span>
+                                                <span>Website (<span className="text-blue-600">{botData?.website_url}</span>)</span>
                                             </Link>
                                         )}
                                     </div>
@@ -177,14 +159,14 @@ export const BotComponent: React.FC = () => {
                         </div>
                     </div>
                 </section>
-                <Feedbacks botid={botid} bot={bot} dev={dev} />
+                <Feedbacks botid={botid} bot={botData} dev={dev} />
             </div>
             <Helmet>
-                <title>Simo Botlist - {bot.name}</title>
-                <meta property="og:title" content={bot.name} />
+                <title>Simo Botlist - {botData.name}</title>
+                <meta property="og:title" content={botData.name} />
                 <meta property="og:type" content="website" />
-                <meta property="og:image" content={`https://cdn.discordapp.com/avatars/${botData.id}/${botData.avatar}.png`} />
-                <meta property="og:description" content={bot.short_description} />
+                <meta property="og:image" content={`https://cdn.discordapp.com/avatars/${botData._id}/${botData.avatar}.png`} />
+                <meta property="og:description" content={botData.short_description} />
                 <meta name="theme-color" content="#FFFFFF" />
             </Helmet>
         </section>
