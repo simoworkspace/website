733b037a0740bd402323e7017ab6c5dfee7dd207
Spyei <caio.salamao1234@gmail.com>
2023-12-05 13:26:25 -0300

    chore: banner and bio in dashboard user

--- src/components/Dashboard/User.tsx
+++ src/components/Dashboard/User.tsx
@@ -1,34 +1,33 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 import { borderColor } from "../../utils/theme/border";
 import { Theme, UserStructure } from "../../types";
-import { Button } from "../Mixed/Button";
-import * as icon from "react-icons/bs";
+import simo from "../../assets/images/simo.png";
 
 export const DashboardUser: FC<{
     color: Theme;
     user: UserStructure;
 }> = ({ color, user }) => {
-    const currentPage: string = location.href.split("/")[4]; 
-    const [page, setPage] = useState<string>("");
-
-    useEffect(() => {
-        setPage(currentPage);
-    }, []);
-
     return (
-        <div className={`${borderColor[color]} border-2 w-[300px] py-8 xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
-            <div>
-                <img className="rounded-full" src={`https://cdn.discordapp.com/avatars/${user?._id || user.id}/${user?.avatar}.png`} alt={`${user?.username}'s Avatar`} />
+        <div className={`${borderColor[color]} border-2 ${user.banner_url ? "min-h-[300px]" : "p-6"} w-[300px] xl:w-[90vw] rounded-lg bg-neutral-900 flex justify-start flex-col gap-4 relative`}>
+            {user.banner_url && (
+                <img className="w-full h-36 object-cover rounded-md z-0 mb-14" src={user.banner_url} alt="Possible banner" />
+            )}
+            <div className={`w-full items-center flex justify-center ${user.banner_url && "z-1 absolute top-36 left-0 transform -translate-y-1/2"}`}>
+                <img
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = simo;
+                    }}
+                    className="rounded-full w-32" src={`https://cdn.discordapp.com/avatars/${user._id || user.id}/${user.avatar}.png`}
+                    alt={`${user.username}'s Avatar`}
+                />
             </div>
-            <hr className="w-[80%] my-5" />
-            <div className="flex flex-col text-center justify-center">
-                <strong>{user?.username}</strong>
-                <span className="text-[#797979] items-center flex text-[13px] justify-center">
-                    ( {user?._id || user.id} )
+            <div className="flex flex-col justify-center gap-1 z-2 relative px-3 pb-4">
+                <strong>{user.username}</strong>
+                <span className="text-[#797979] items-center flex text-[13px]">
+                    ( {user._id || user.id} )
                 </span>
-            </div>
-            <div className="mt-8 w-10/12">
-                {!page ? <Button clas="text-center flex items-center gap-2 w-full flex items-center justify-center" link to={"/dashboard/settings"}><icon.BsGear/>Configurações</Button> : <Button clas="text-center flex items-center gap-2 w-full flex items-center justify-center" link to={page === "settings" ? "/dashboard" : "/dashboard/settings"}><icon.BsArrowLeft />Voltar</Button>}
+                {user?.bio && <span className="mt-5">{user.bio}</span>}
             </div>
         </div>
     )
