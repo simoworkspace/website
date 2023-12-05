0e363b1c4fd4eb435c39fc0a99bc2adb3bc2e78a
Spyei <caio.salamao1234@gmail.com>
2023-12-05 13:55:16 -0300

    chore: 404 page

--- src/pages/Mixed/NotFound.tsx
+++ src/pages/Mixed/NotFound.tsx
@@ -1,33 +1,22 @@
-import React, { useContext, useEffect, useState } from "react";
-import { buttonColor } from '../../utils/theme/button';
-import { ThemeContext } from "../../contexts/ThemeContext";
-import { borderColor } from "../../utils/theme/border";
-import  axios, { AxiosResponse } from "axios";
+import { FC } from "react";
+import { Button } from "../../components/Mixed/Button";
 
-export const NotFound: React.FC = () => {
-    const { color } = useContext(ThemeContext);
-    const [cat, setCat] = useState<string>();
-
-    const generateCat = async (): Promise<void> => {
-        const req: AxiosResponse<[{ url: string }]> = await axios.get("https://api.thecatapi.com/v1/images/search");
-        setCat(req.data[0].url);
-    };
-
-    useEffect(() => {
-        generateCat();
-    }, []);
-
+export const NotFound: FC = () => {
     return (
-        <section className="w-screen xl:w-[90vw] h-[600px] max-w-[1500px] flex items-center justify-center text-white flex-col">
-            <div className={`border-2 ${borderColor[color]} flex flex-col items-center justify-center text-center break-before-all p-[50px] xl:p-10 rounded-lg bg-neutral-900 gap-4`}>
-                <span className="text-[40px] xl:text-[22px]"><strong>Página não encontrada.</strong></span>
-                <span className="text-[22px] xl:text-[16px]">Nossos anões digitais procuraram por todos os cantos, mas não conseguiram encontrar a página que você está buscando.</span>
-                {cat ? (
-                    <img  className="w-[300px] h-[200px] object-contain" src={cat} />
-                ) : (
-                    <div className="bg-neutral-800 animate-pulse w-[300px] h-[200px]"></div>
-                )}
-                <button onClick={() => location.href = "/"} className={`${buttonColor[color]} transition-colors duration-300 p-3 rounded-lg border-2`}>Take me back</button>
+        <section className="w-screen xl:w-[90vw] p-8 max-w-[1500px] flex items-center justify-center text-white flex-col">
+            <div className="flex flex-col xl:flex-col items-center xl:justify-center xl:text-center justify-start text-start break-before-all p-12 xl:p-10 rounded-lg gap-4">
+                <span className="text-[40px] xl:text-[22px]"><strong>404 NOT_F0UND</strong></span>
+                <div>
+                    <img className="w-[300px] h-[200px] object-contain" src="https://cdn.discordapp.com/attachments/993955863150727289/1174504430012481637/Z.png?ex=657a4a7d&is=6567d57d&hm=c1e748a3f8b9babdb33fa0ca062f3da500b436ded54f9b02a81b923da0f9fe3d&" />
+                </div>
+                <div className="flex flex-col gap-3">
+                    <span className="text-[20px] xl:text-[16px]">Página não encontrada.. mas você já comprou Simo Gomas hoje?</span>
+                    <div className="w-full mt-2 flex justify-center items-center">
+                        <div className="w-[200px] flex items-center">
+                            <Button link clas="text-center">Take me back</Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
