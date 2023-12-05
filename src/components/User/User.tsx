c156726dc6283068e97a1799d86efb8711d4ec38
Spyei <caio.salamao1234@gmail.com>
2023-12-05 13:27:00 -0300

    fix: user when have no banner

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import api from "../../utils/api";
-import { Link, Params, useParams } from "react-router-dom";
-import { BotStructure, DBUser, Team } from "../../types";
+import { Params, useParams } from "react-router-dom";
+import { BotStructure, DBUser } from "../../types";
 import { AxiosResponse } from "axios";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import { borderColor } from "../../utils/theme/border";
@@ -16,17 +16,19 @@ export const User: React.FC = () => {
     const [userBots, setUserBots] = useState<BotStructure[]>([]);
     const { color } = useContext(ThemeContext);
 
-    const getUserData: () => Promise<void> = async (): Promise<void> => {
-        const req: AxiosResponse<DBUser> = await api.getUserFromDB(userid);
-        return setUser(req.data);
+    const getUserData = async () => {
+        const { data } = await api.getUserFromDB(userid);
+
+        return setUser(data);
     };
 
-    const getUserBots: () => Promise<void> = async (): Promise<void> => {
-        const req: AxiosResponse<BotStructure[]> = await api.getAllBots();
-        const userBots = req.data.filter((bot: BotStructure) => bot.owner_id === userid);
+    const getUserBots = async () => {
+        const { data } = await api.getAllBots();
+        const userBots = data.filter((bot) => bot.owner_id === userid);
+
         return setUserBots(userBots);
     };
-
+    
     useEffect(() => {
         getUserData();
         getUserBots();
@@ -38,11 +40,11 @@ export const User: React.FC = () => {
                 <UserLoading />
             ) : (
                 <section className="w-screen flex flex-row p-5 text-white items-start xl:items-center justify-center gap-10 xl:flex-col">
-                    <div className={`${borderColor[color]} border-2 min-h-[300px] w-[300px] xl:w-[90vw] rounded-lg bg-neutral-900 flex justify-start flex-col gap-4 relative`}>
+                    <div className={`${borderColor[color]} border-2 ${user.banner_url ? "min-h-[300px]" : "p-6"} w-[300px] xl:w-[90vw] rounded-lg bg-neutral-900 flex justify-start flex-col gap-4 relative`}>
                         {user.banner_url && (
                             <img className="w-full h-36 object-cover rounded-md z-0 mb-14" src={user.banner_url} alt="Possible banner" />
                         )}
-                        <div className="w-full items-center flex justify-center z-1 absolute top-[50%] left-0 transform -translate-y-1/2">
+                        <div className={`w-full items-center flex justify-center ${user.banner_url && "z-1 absolute top-[50%] left-0 transform -translate-y-1/2"}`}>
                             <img
                                 onError={({ currentTarget }) => {
                                     currentTarget.onerror = null;
@@ -59,7 +61,6 @@ export const User: React.FC = () => {
                             </span>
                         </div>
                     </div>
-
                     <div className="flex items-start w-full flex-col gap-2">
                         <h1 className="text-[33px]">Perfil de <strong>{user.username}</strong></h1>
                         {user?.bio && <span>{user.bio}</span>}
