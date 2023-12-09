31d60af58695ef7683f9b4b54ea905c4752eb480
Spyei <caio.salamao1234@gmail.com>
2023-12-09 10:52:14 -0300

    feat: copy and re-genereate your api key

--- src/components/Dashboard/Bot.tsx
+++ src/components/Dashboard/Bot.tsx
@@ -11,6 +11,7 @@ import { buttonColor } from "../../utils/theme/button";
 import { Link } from "react-router-dom";
 import { DeleteBot } from "./Delete";
 import { borderAndBg } from "../../utils/theme/border&bg";
+import { Button } from "../Mixed/Button";
 
 export const DashboardBot: React.FC = () => {
     const { color } = useContext(ThemeContext);
@@ -22,16 +23,13 @@ export const DashboardBot: React.FC = () => {
     const [apiKeyLoading, setApiKeyLoading] = useState<boolean>(false);
     const [deleteBot, setDeleteBot] = useState<boolean>(false);
 
-    const handleCreateApiKey = async (botId: string): Promise<void> => {
-        if (apiKey) {
-            await navigator.clipboard.writeText(apiKey);
-            return alert("Copiado apikey para área de transferência.")
-        };
-
+    const handleCreateApiKey = async () => {
         setApiKeyLoading(true);
-        const req = await api.createApiKey(botId);
-        setApiKey(req.data.api_key);
-        return setApiKeyLoading(false);
+
+        const { data: { api_key } } = await api.createApiKey(selectedBot?._id as string);
+        setApiKey(api_key);
+
+        setApiKeyLoading(false);
     };
 
     const getUserBots = async () => {
@@ -44,6 +42,12 @@ export const DashboardBot: React.FC = () => {
         return setBots(data ? data : null);
     };
 
+    const getBotApiKey = async () => {
+        const { data: { api_key } } = await api.getApiKey(selectedBot?._id as string);
+
+        setApiKey(api_key || "");
+    };
+
     const getSelectedBot = (botId: string) => {
         const selbot = bots?.find(bot => bot._id == botId);
         setSelectBotMenu(false);
@@ -51,10 +55,10 @@ export const DashboardBot: React.FC = () => {
     };
 
     useEffect(() => {
-        if (user) {
-            getUserBots();
-        }
-    }, [user]);
+        if (user) getUserBots();
+
+        if (selectedBot) getBotApiKey();
+    }, [user, selectedBot]);
 
     return (
         <main className="flex items-center justify-start h-full w-full flex-col">
@@ -91,41 +95,45 @@ export const DashboardBot: React.FC = () => {
             </div>
             {selectedBot && (
                 <section className={`w-full bg-neutral-900 mt-2 border-2 flex-row ${borderColor[color]} rounded-lg p-4`}>
-                    <div className="grid grid-cols-2 xl:grid-cols-1 gap-8 text-white m-2">
-                        <div className={`bg-neutral-900 w-full border-neutral-800 border-2 rounded-lg p-3 transition-colors hover:bg-[#1d1d1d] flex flex-col gap-3`} key={selectedBot._id}>
-                            <Link to={`/bot/${selectedBot._id}`} className="flex flex-col gap-3">
-                                <div className="flex gap-2 items-center">
-                                    <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${selectedBot._id}/${selectedBot.avatar}.png`} />
-                                    <div className="flex gap-2 flex-col">
-                                        <span className="font-bold text-lg">{selectedBot.name}</span>
+                    <div className="flex flex-col gap-2">
+                        <div className="grid grid-cols-2 xl:grid-cols-1 gap-8 text-white m-2">
+                            <div className={`bg-neutral-900 w-full border-neutral-800 border-2 rounded-lg p-3 transition-colors hover:bg-[#1d1d1d] flex flex-col gap-3`} key={selectedBot._id}>
+                                <Link to={`/bot/${selectedBot._id}`} className="flex flex-col gap-3">
+                                    <div className="flex gap-2 items-center">
+                                        <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${selectedBot._id}/${selectedBot.avatar}.png`} />
+                                        <div className="flex gap-2 flex-col">
+                                            <span className="font-bold text-lg">{selectedBot.name}</span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="flex gap-3 flex-col">
-                                    <span className="min-h-[48px] break-before-auto">{selectedBot.short_description}</span>
-                                    <div className="flex flex-row gap-1 flex-wrap">
-                                        {selectedBot.tags.map((tag, index) => (
-                                            <div key={index} className={`${borderAndBg[color]} p-[6px] rounded-lg border-2`}>{tag}</div>
-                                        ))}
+                                    <div className="flex gap-3 flex-col">
+                                        <span className="min-h-[48px] break-before-auto">{selectedBot.short_description}</span>
+                                        <div className="flex flex-row gap-1 flex-wrap">
+                                            {selectedBot.tags.map((tag, index) => (
+                                                <div key={index} className={`${borderAndBg[color]} p-[6px] rounded-lg border-2`}>{tag}</div>
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
-                        </div>
-                        <div className="w-full flex flex-col gap-3">
-                            <button onClick={() => setDeleteBot(true)} className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor["red"]} h-12 transition-colors duration-300 border-2`}>
-                                <iconBS.BsTrashFill />
-                                <span>Deletar bot</span>
-                            </button>
-                            <Link to={`/dashboard/edit/${selectedBot._id}`} className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor[color]} h-12 transition-colors duration-300 border-2`}>
-                                <iconMD.MdEditSquare />
-                                <span>Editar bot</span>
-                            </Link>
-                            <div className="flex flex-row xl:flex-col bg-neutral-800 w-full h-full rounded-lg items-center">
-                                <input onAuxClick={async () => {
-                                    await navigator.clipboard.writeText(apiKey);
-                                }} disabled value={apiKey} placeholder="Clique no botão para solicitar uma api key" className="flex-grow p-2 bg-transparent xl:break-words" />
-                                <button onClick={() => handleCreateApiKey(selectedBot._id)} className={`${buttonColor[color]} h-full border-2 transition-colors duration-300 rounded-r-lg p-2 xl:w-full xl:rounded-lg flex items-center justify-center`}>
-                                    {apiKeyLoading ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={30} className="animate-spin" /> : apiKey ? <iconMD.MdOutlineContentCopy fill="#fff" size={22} /> : "Gerar"}
+                                </Link>
+                            </div>
+                            <div className="w-full flex flex-col gap-3 items-center justify-center">
+                                <button onClick={() => setDeleteBot(true)} className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor["red"]} h-12 transition-colors duration-300 border-2`}>
+                                    <iconBS.BsTrashFill />
+                                    <span>Deletar bot</span>
                                 </button>
+                                <Link to={`/dashboard/edit/${selectedBot._id}`} className={`flex flex-row items-center justify-center gap-3 p-3 w-full rounded-lg ${buttonColor[color]} h-12 transition-colors duration-300 border-2`}>
+                                    <iconMD.MdEditSquare />
+                                    <span>Editar bot</span>
+                                </Link>
+                            </div>
+                        </div>
+                        <span className="text-lg font-bold">Chave de api</span>
+                        <div className="flex flex-row xl:flex-col bg-neutral-800 w-full rounded-lg items-center">
+                            <input disabled value={apiKey} placeholder="Solicite uma nova api key" className="flex-grow p-2 bg-transparent xl:w-full" />
+                            <div className="flex xl:w-full">
+                                <Button clas="rounded-r-none" action={async () => await navigator.clipboard.writeText(apiKey)}>
+                                    <iconMD.MdOutlineContentCopy fill="#fff" size={24} />
+                                </Button>
+                                <Button disabled={apiKeyLoading} action={handleCreateApiKey} clas="h-full xl:w-full items-center justify-center rounded-l-none">{apiKeyLoading ? <iconAI.AiOutlineLoading3Quarters fill="#fff" size={30} className="animate-spin" /> : apiKey ? "Atualizar" : "Gerar"}</Button>
                             </div>
                         </div>
                     </div>
