9189ae182b899cface0e2a07a98aefb0c24e9ff3
Spyei <caio.salamao1234@gmail.com>
2023-12-13 17:42:56 -0300

    fix: getting user when have cookies

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from "react";
 import { UserStructure } from "../types";
 import api from "../utils/api";
+import Cookies from "js-cookie";
 
 interface UserContextProps {
     user: UserStructure | null;
@@ -9,17 +10,21 @@ interface UserContextProps {
 
 export const UserContext = createContext<UserContextProps>({
     user: null,
-    setUser: () => void 1,
+    setUser: () => void 0,
 });
 
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }: React.PropsWithChildren) => {
     const [user, setUser] = useState<UserStructure | null>(null);
 
     const getUserData = async () => {
-        const { data } = await api.getUserData();
+        if (Cookies.get("discordUser")) {
+            const { data } = await api.getUserData();
 
-        if (data) {
-            setUser(data);
+            if (data) {
+                setUser(data);
+            } else {
+                setUser(null);
+            }
         } else {
             setUser(null);
         }
