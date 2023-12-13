1f7c0aab97eaf7e5d4a521641122be98e6c1aead
Spyei <caio.salamao1234@gmail.com>
2023-12-13 17:42:32 -0300

    chore: removing auth component from app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -48,14 +48,14 @@ function App() {
                     <Route path="/search" element={<Search />} />
                     <Route path="/notifications" element={<NotificationsPage />} />
                     <Route path="/themes" element={<ThemesPage />} />
-                    <Route path="/addbot" element={<Auth><Addbot /></Auth>} />
+                    <Route path="/addbot" element={<Addbot />} />
                     <Route path="/user/:userid" element={<User />} />
-                    <Route path="/dashboard" element={<Auth><Dashboard /></Auth>} />
-                    <Route path="/dashboard/edit/:botId" element={<Auth><DashboardEditPage /></Auth>} />
-                    <Route path="/team/create" element={<Auth><CreateTeamPage /></Auth>} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard/edit/:botId" element={<DashboardEditPage />} />
+                    <Route path="/team/create" element={<CreateTeamPage />} />
                     <Route path="/team/:teamId" element={<TeamPage />} />
-                    <Route path="/team/:teamId/invite/:hash" element={<Auth><InvitePage /></Auth>} />
-                    <Route path="/team/manage/:teamId" element={<Auth><ManageTeamPage /></Auth>} />
+                    <Route path="/team/:teamId/invite/:hash" element={<InvitePage />} />
+                    <Route path="/team/manage/:teamId" element={<ManageTeamPage />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
                 <Mobilemenu />
