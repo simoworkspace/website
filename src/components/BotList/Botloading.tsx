acb6076bd71ac6eaf01ba0599961983d957344b9
Spyei <caio.salamao1234@gmail.com>
2023-12-13 15:12:02 -0300

    feat: better bot card loading

--- src/components/BotList/Botloading.tsx
+++ src/components/BotList/Botloading.tsx
@@ -1,28 +1,80 @@
+
+import { TiArrowSortedUp } from "react-icons/ti";
+
+
 export const Botloading: React.FC<{ fills: number, grid?: boolean }> = ({ fills, grid }) => {
     return grid ? (
         <>
             {Array(fills).fill(
-                <div className="border-2 bg-card-blue rounded-[10px] bg-neutral-950 xl:w-[95vw]">
-                    <div className="bg-neutral-900 xl:w-[80px] xl:h-[80px] w-[100px] h-[100px] float-right rounded-full mt-[10px] mr-[10px] self-center animate-pulse"></div>
-                    <div className="bg-neutral-900 xl:w-[50%] w-[220px] h-[26px] rounded-[10px] flex ml-20 xl:ml-10 animate-pulse"></div>
-                    <div className="bg-neutral-900 xl:w-[65%] w-[70%] h-[72px] mt-3 rounded-[10px] ml-3 animate-pulse"></div>
-                    <div className="m-[10px] flex ">
-                        <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
-                        <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
+                <div className={`bg-neutral-900 w-full rounded-lg p-3 transition-colors border-[#1d1d1d] border-2 flex flex-col gap-3 xl:w-[95vw]`}>
+                    <div className="flex flex-col gap-3">
+                        <div className="flex gap-2 items-center">
+                            <div className="rounded-full w-12 h-12 animate-pulse bg-neutral-800"></div>
+                            <div className="flex gap-2 flex-col">
+                                <div className="h-6 w-24 bg-neutral-800 animate-pulse rounded-full"></div>
+                                <div className="flex">
+                                    <TiArrowSortedUp fill="#fff" size={20} />
+                                    <div className="animate-pulse bg-neutral-800 h-4 w-6 rounded-lg"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex gap-3 flex-col">
+                            <div className="flex flex-col gap-2">
+                                <div className="bg-neutral-800 animate-pulse h-5 w-full rounded-full"></div>
+                                <div className="bg-neutral-800 animate-pulse h-5 w-3/5 rounded-full"></div>
+                            </div>
+                            <div className="flex flex-row gap-1 flex-wrap">
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-8 rounded-lg border-2 animate-pulse"></div>
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-12 rounded-lg border-2 animate-pulse"></div>
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-6 rounded-lg border-2 animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex gap-3 w-full">
+                        <div className="flex w-full flex-grow">
+                            <div className="animate-pulse text-center border-2 border-neutral-700 h-[52px] transition-all duration-300 p-3 rounded-lg w-full bg-neutral-800"></div>
+                        </div>
+                        <div>
+                            <div className="animate-pulse text-center border-2 border-neutral-700 h-[52px] transition-all duration-300p-3 rounded-lg px-5 flex gap-2 items-center justify-center w-full bg-neutral-800 text-neutral-800">dsajkdalj</div>
+                        </div>
                     </div>
                 </div>
             )}
         </>
     ) : (
-        <div className="grid-cols-2 gap-8 m-2 grid xl:grid-cols-1 w-screen max-w-[1500px] xl:flex xl:flex-wrap xl:justify-center">
+        <div className="grid-cols-2 grid gap-3 text-white p-3 xl:w-full xl:grid-cols-1 w-full">
             {Array(fills).fill(
-                <div className="border-2 bg-card-blue rounded-[10px] bg-neutral-950 xl:w-[95vw]">
-                    <div className="bg-neutral-900 xl:w-[80px] xl:h-[80px] w-[100px] h-[100px] float-right rounded-full mt-[10px] mr-[10px] self-center animate-pulse"></div>
-                    <div className="bg-neutral-900 xl:w-[50%] w-[220px] h-[26px] rounded-[10px] flex ml-20 xl:ml-10 animate-pulse"></div>
-                    <div className="bg-neutral-900 xl:w-[65%] w-[70%] h-[72px] mt-3 rounded-[10px] ml-3 animate-pulse"></div>
-                    <div className="m-[10px] flex ">
-                        <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
-                        <div className="bg-neutral-900 w-[170px] h-[40px] m-2 rounded-lg animate-pulse"></div>
+                <div className="bg-neutral-900 w-full rounded-lg p-3 transition-colors border-[#1d1d1d] border-2 flex flex-col gap-3 xl:w-[95vw]">
+                    <div className="flex flex-col gap-3">
+                        <div className="flex gap-2 items-center">
+                            <div className="rounded-full w-12 h-12 animate-pulse bg-neutral-800"></div>
+                            <div className="flex gap-2 flex-col">
+                                <div className="h-6 w-24 bg-neutral-800 animate-pulse rounded-full"></div>
+                                <div className="flex">
+                                    <TiArrowSortedUp fill="#fff" size={20} />
+                                    <div className="animate-pulse bg-neutral-800 h-4 w-6 rounded-lg"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex gap-3 flex-col">
+                            <div className="flex flex-col gap-2">
+                                <div className="bg-neutral-800 animate-pulse h-5 w-full rounded-full"></div>
+                                <div className="bg-neutral-800 animate-pulse h-5 w-3/5 rounded-full"></div>
+                            </div>
+                            <div className="flex flex-row gap-1 flex-wrap">
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-8 rounded-lg border-2 animate-pulse"></div>
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-12 rounded-lg border-2 animate-pulse"></div>
+                                <div className="bg-neutral-800 border-neutral-700 py-4 px-6 rounded-lg border-2 animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex gap-3 w-full">
+                        <div className="flex w-full flex-grow">
+                            <div className="animate-pulse text-center border-2 border-neutral-700 h-[52px] transition-all duration-300 p-3 rounded-lg w-full bg-neutral-800"></div>
+                        </div>
+                        <div>
+                            <div className="animate-pulse text-center border-2 border-neutral-700 h-[52px] transition-all duration-300p-3 rounded-lg px-5 flex gap-2 items-center justify-center w-full bg-neutral-800 text-neutral-800">dsajkdalj</div>
+                        </div>
                     </div>
                 </div>
             )}
