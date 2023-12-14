5aa65fa7e00d0d9fd4ae8f3845d3d78fa84f640f
Spyei <caio.salamao1234@gmail.com>
2023-12-13 21:15:12 -0300

    feat: better bot loading

--- src/components/Bot/BotLoading.tsx
+++ src/components/Bot/BotLoading.tsx
@@ -1,33 +1,44 @@
-import React from "react";
+import { FC } from "react";
+import * as icon from "react-icons/bs";
 
-export const BotLoading: React.FC = () => {
+export const BotLoading: FC = () => {
     return (
         <div className="w-screen max-w-[1500px]">
             <div className="flex flex-col items-center justify-center">
                 <section className="flex items-center xl:flex-col justify-center w-full xl:mt-2 mt-[30px] text-white">
-                    <div className="bg-neutral-900 rounded-xl flex xl:flex-col xl:h-[320px] h-[120px] w-[95%] border-2 items-center justify-center">
-                        <div className="w-[220px] h-[100px] xl:my-2 rounded-full xl:float-none ml-2 bg-neutral-800 animate-pulse"></div>
-                        <div className="flex flex-col w-full justify-center gap-2">
-                            <div className="ml-6 xl:m-0 xl:my-1 text-white flex xl:flex-col xl:items-center flex-row gap-3 text-[26px] w-[400px] h-[60px] rounded-lg bg-neutral-800 animate-pulse">
-                                <span className="text-[#797979] items-center flex text-[13px]"></span>
+                    <div className="bg-neutral-900 rounded-xl flex xl:flex-col xl:h-[320px] h-[120px] w-[95%] border-2 border-neutral-800 items-center justify-center">
+                        <div className="w-[220px] h-[100px] xl:w-[100px] xl:my-2 ml-3 xl:ml-0 rounded-full bg-neutral-800 animate-pulse"></div>
+                        <div className="flex flex-col w-full justify-center gap-2 xl:items-center">
+                            <div className="ml-6 xl:m-0 xl:my-1 flex xl:flex-col xl:items-center flex-row gap-3">
+                                <div className="rounded-full animate-pulse bg-neutral-800 w-56 h-10"></div>
+                                <div className="rounded-full animate-pulse bg-neutral-800 w-40 h-6"></div>
                             </div>
+                            <div className="rounded-full animate-pulse bg-neutral-800 w-32 h-6 ml-6 xl:ml-0"></div>
                         </div>
                         <div className="flex w-full justify-end ">
                             <div className="flex gap-4 items-center justify-center xl:w-screen flex-row m-4">
-                                <a className="border-2 border-neutral-700 bg-neutral-900 text-white hover:bg-neutral-700 transition-colors duration-300 p-2 rounded-md w-[120px] text-center">
-                                    <span>Votar</span>
-                                </a>
-                                <a className="border-2 border-neutral-700 bg-neutral-900 text-white hover:bg-neutral-700 transition-colors duration-300 p-2 rounded-md w-[120px] text-center">
-                                    <span>Adicionar</span>
-                                </a>
+                                <div className="h-10 bg-neutral-800 animate-pulse hover:bg-neutral-800 transition-colors duration-300 p-2 rounded-md w-[120px] text-center">
+                                </div>
+                                <div className="bg-neutral-800 animate-pulse h-10 hover:bg-neutral-800 transition-colors duration-300 p-2 rounded-md w-[120px] text-center">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </section>
-                <section className="w-[90%] mb-5 bg-neutral-900 border-2 border-t-0 rounded-t-none rounded-lg p-10 xl:p-3">
+                <section className="w-[90%] mb-5 bg-neutral-900 border-2 border-neutral-800 border-t-0 rounded-t-none rounded-lg p-10 xl:p-3">
                     <div className="flex flex-row xl:flex-col">
-                        <div className="w-[80%] xl:w-full flex break-words xl:justify-center">
-                            <div className="w-[93%] bg-neutral-800 rounded-lg animate-pulse"></div>
+                        <div className="w-[80%] xl:w-full flex flex-col gap-3">
+                            <div className="bg-neutral-800 rounded-full w-[30%] h-12"></div>
+                            <div className="bg-neutral-800 rounded-full w-[93%] h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[86%] h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[36%] mb-3 h-6"></div>
+                            <div className="bg-neutral-800 rounded-lg w-[38%] h-44"></div>
+                            <div className="bg-neutral-800 rounded-full w-[93%] h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[93%] h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[43%] mb-3 h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[56%] h-12"></div>
+                            <div className="bg-neutral-800 rounded-full w-[93%] h-6"></div>
+                            <div className="bg-neutral-800 rounded-full w-[53%] h-6"></div>
                         </div>
                         <div className="w-[1px] bg-[#8b8b8b]" />
                         <hr className="xl:my-4 xl:w-full" />
@@ -36,32 +47,47 @@ export const BotLoading: React.FC = () => {
                                 <div className="w-full">
                                     <h1 className="text-2xl text-center">Developer</h1>
                                     <hr className="my-4 w-full" />
-                                    <div>
-                                        <a className="bg-neutral-800 p-2 rounded-lg flex flex-row xl:flex-col items-center gap-4 transition-colors duration-300 hover:bg-neutral-800 w-full h-[80px] animate-pulse"></a>
+                                    <div className="bg-neutral-900 w-56 border-neutral-800 border-2 p-2 rounded-lg justify-center flex flex-row items-center gap-4 transition-colors duration-300 h-[80px] animate-pulse">
+                                        <div className="w-16 h-16 rounded-full bg-neutral-800 animate-pulse"></div>
+                                        <div className="w-28 h-6 rounded-full bg-neutral-800 animate-pulse"></div>
                                     </div>
                                 </div>
                                 <div>
                                     <h1 className="text-2xl text-center mt-2">Informações</h1>
                                     <hr className="my-4 w-full" />
                                     <div className="flex flex-col w-full gap-3">
-                                        <div>
-                                            <strong className="text-lg">Prefixo</strong> <span>...</span>
+                                        <div className="flex gap-2 items-center">
+                                            <strong className="text-lg">Prefixo</strong>
+                                            <div className="bg-neutral-800 animate-pulse h-6 w-12 rounded-lg"></div>
                                         </div>
-                                        <div>
-                                            <strong className="text-lg">Votos </strong><span>...</span>
+                                        <div className="flex gap-2 items-center">
+                                            <strong className="text-lg">Votos </strong>
+                                            <div className="bg-neutral-800 animate-pulse h-6 w-7 rounded-lg"></div>
                                         </div>
                                         <div>
                                             <div className="flex flex-row gap-3 flex-wrap">
                                                 <strong className="text-lg">Tags</strong>
-                                                <div className="p-[6px] rounded-lg bg-neutral-800 w-[80%] h-[65px] animate-pulse"></div>
+                                                <div className="p-4 px-12 rounded-lg bg-neutral-800 border-2 border-neutral-700 animate-pulse"></div>
+                                                <div className="p-4 px-16 rounded-lg bg-neutral-800 border-2 border-neutral-700 animate-pulse"></div>
+                                                <div className="p-4 px-8 rounded-lg bg-neutral-800 border-2 border-neutral-700 animate-pulse"></div>
+                                                <div className="p-4 px-10 rounded-lg bg-neutral-800 border-2 border-neutral-700 animate-pulse"></div>
                                             </div>
                                         </div>
                                         <h1 className="text-2xl text-center">Links</h1>
                                         <hr className="my-1 w-full" />
-                                        <div className="flex flex-col gap-3 flex-wrap">
-                                            <a className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></a>
-                                            <a className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></a>
-                                            <a className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></a>
+                                        <div className="flex flex-col gap-4">
+                                            <div className="flex gap-2 items-center">
+                                                <icon.BsDiscord size={30} fill="#5662F6" />
+                                                <div className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></div>
+                                            </div>
+                                            <div className="flex gap-2 items-center">
+                                                <icon.BsGithub size={30} />
+                                                <div className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></div>
+                                            </div>
+                                            <div className="flex gap-2 items-center">
+                                                <icon.BsGlobe size={30} />
+                                                <div className="flex items-center gap-3 p-2 w-[80%] h-[30px] bg-neutral-800 rounded-full"></div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -78,7 +104,7 @@ export const BotLoading: React.FC = () => {
                         <div className="bg-neutral-800 animate-pulse w-[300px] h-[39px] rounded-full my-2"></div>
                         <div className="flex flex-col gap-3">
                             {Array(3).fill(
-                                <div className="bg-neutral-900 w-full h-[150px] rounded-lg border-2">
+                                <div className="bg-neutral-900 w-full h-[150px] rounded-lg border-2 border-neutral-800">
                                     <div className="flex flex-col p-3 gap-2">
                                         <div className="flex flex-row items-center justify w-full">
                                             <div className="w-[40px] h-[40px] rounded-full animate-pulse bg-neutral-800"></div>
