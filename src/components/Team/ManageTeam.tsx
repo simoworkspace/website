867ed2609289a4652b7562445e3454bc5a99b624
Spyei <caio.salamao1234@gmail.com>
2023-12-15 13:29:31 -0300

    feat: dinamically update team infos

--- src/components/Team/ManageTeam.tsx
+++ src/components/Team/ManageTeam.tsx
@@ -32,9 +32,50 @@ export const ManageTeamComponent: FC = () => {
     const [error, setError] = useState<ErrorStructure>();
     const [logs, setLogs] = useState<AuditLogStructure>();
 
+    const [editActions, setEditActions] = useState<{
+        changesLoading?: boolean;
+        changesMade?: boolean;
+        description: string | null;
+        avatar_url: string;
+        name: string;
+    }>({
+        avatar_url: team?.avatar_url || "",
+        description: team?.description || "",
+        name: team?.name || "",
+        changesLoading: false,
+        changesMade: false
+    });
+
+    const updateTeam = async () => {
+        try {
+            setEditActions({ changesLoading: true, changesMade: true, avatar_url: editActions.avatar_url, description: editActions.description, name: editActions.name });
+
+            const updatedTeamData = {
+                avatar_url: editActions.avatar_url,
+                description: editActions.description,
+                name: editActions.name
+            };
+
+            //@ts-ignore
+            await api.patchTeam(teamID, updatedTeamData);
+            await getAuditLogs();
+
+            setEditActions({ changesMade: false, changesLoading: false, avatar_url: editActions.avatar_url, description: editActions.description, name: editActions.name });
+        } catch(error: any) {
+            setError({
+                show: true,
+                title: "Erro ao tentar adicionar um bot",
+                message: ApiErrors[error.response.data.errors] || JSON.stringify(error.response.data.errors[0])
+            });
+            
+            if (team) setEditActions({ changesLoading: false, changesMade: false, avatar_url: team.avatar_url, description: team.description, name: team.name });
+        }
+    };
+
     const getTeam = async () => {
         const { data } = await api.getTeam(teamID);
 
+        setEditActions({ description: data.description, avatar_url: data.avatar_url, name: data.name });
         return setTeam(data);
     };
 
@@ -54,35 +95,35 @@ export const ManageTeamComponent: FC = () => {
             <section className="w-screen flex flex-row p-5 text-white items-start xl:items-center justify-start gap-10 xl:flex-col h-full">
                 {team ? (
                     <div className={`${borderColor[color]} border-2 w-[300px] p-5 xl:w-[90vw] rounded-lg bg-neutral-900 flex items-center justify-center flex-col`}>
-                    <div>
-                        <img onError={({ currentTarget }) => {
-                            currentTarget.onerror = null;
-                            currentTarget.src = simo;
-                        }}
-                            className="rounded-full w-32 h-32 object-center" src={team.avatar_url} />
-                    </div>
-                    <hr className="w-[80%] my-6" />
-                    <div className="flex flex-col text-center justify-center">
-                        <strong>{team.name}</strong>
-                        <span className="text-[#797979] items-center flex text-[13px] justify-center">
-                            ( {team.id} )
-                        </span>
-                    </div>
-                    <div className="flex w-full flex-col gap-3 py-3 px-5">
-                        <span className="text-lg font-bold text-left">Membros</span>
-                        <div className="flex flex-wrap w-full gap-2">
-                            {team.members?.map((member, index) => (
-                                <Link className="relative" key={index} to={`/user/${member.id}`}>
-                                    {member.permission === 2 && <iconBI.BiSolidCrown fill="#FFD700" className="absolute ml-7 rotate-45" />}
-                                    <img
-                                        className="rounded-full w-10"
-                                        src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png?size=2048`}
-                                    />
-                                </Link>
-                            ))}
+                        <div>
+                            <img onError={({ currentTarget }) => {
+                                currentTarget.onerror = null;
+                                currentTarget.src = simo;
+                            }}
+                                className="rounded-full w-32 h-32 object-center" src={editActions.avatar_url} />
+                        </div>
+                        <hr className="w-[80%] my-6" />
+                        <div className="flex flex-col text-center justify-center">
+                            <strong className="max-w-[200px]">{editActions.name}</strong>
+                            <span className="text-[#797979] items-center flex text-[13px] justify-center">
+                                ( {team.id} )
+                            </span>
+                        </div>
+                        <div className="flex w-full flex-col gap-3 py-3 px-5">
+                            <span className="text-lg font-bold text-left">Membros</span>
+                            <div className="flex flex-wrap w-full gap-2">
+                                {team.members?.map((member, index) => (
+                                    <Link className="relative" key={index} to={`/user/${member.id}`}>
+                                        {member.permission === 2 && <iconBI.BiSolidCrown fill="#FFD700" className="absolute ml-7 rotate-45" />}
+                                        <img
+                                            className="rounded-full w-10"
+                                            src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png?size=2048`}
+                                        />
+                                    </Link>
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
                 ) : (
                     <div>Carregando...</div>
                 )}
@@ -103,7 +144,12 @@ export const ManageTeamComponent: FC = () => {
                             <TabIndicator className={`mt[-1.5px] h-[2px] ${borderColor[color]} border-2 rounded-lgx xl:invisible`} />
                             <TabPanels>
                                 <TabPanel>
-                                    <EditTeam team={team} />
+                                    {team ? (
+                                        <EditTeam editActions={editActions} setEditActions={setEditActions} teamID={teamID} team={team} />
+
+                                    ) : (
+                                        <span>Carregando...</span>
+                                    )}
                                 </TabPanel>
                                 <TabPanel>
                                     <ManageMembers updateAuditLogs={getAuditLogs} color={color} />
@@ -131,6 +177,19 @@ export const ManageTeamComponent: FC = () => {
                 </div>
             </section>
             {error?.show && <PopUpError setShow={setError} show={error} />}
+            {(editActions.changesMade && team) && (
+                <div className={`${editActions.changesMade ? "bounceIn" : "invisible"} w-[90vw] bottom-5 origin-center absolute xl:z-10 xl:fixed xl:bottom-10 xl:w-[95vw] bg-neutral-800 ${borderColor[color]} border-2 rounded-lg duration-200`}>
+                    <div className="flex p-2 text-white w-full items-center">
+                        <span className="flex flex-grow">Você tem alterações para serem salvas</span>
+                        <div className="flex gap-2 items-center">
+                            <button onClick={() => setEditActions({ changesMade: false, avatar_url: team.avatar_url, description: team.description, name: team.name })} className="text-neutral-400">Desfazer</button>
+                            <Button disabled={editActions.changesLoading} clas="disabled:opacity-50" action={updateTeam}>
+                                {!editActions.changesLoading ? <span>Salvar alterações</span> : <icon.AiOutlineLoading3Quarters fill="#fff" size={22} className="animate-spin" />}
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </main>
     )
 };
