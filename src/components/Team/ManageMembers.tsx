ba6acc637b129f4f13fe5ba6d9c766dfa4fd366d
Spyei <caio.salamao1234@gmail.com>
2023-12-15 16:27:26 -0300

    chore: better team manage members

--- src/components/Team/ManageMembers.tsx
+++ src/components/Team/ManageMembers.tsx
@@ -29,14 +29,19 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
 
     const [error, setError] = useState<ErrorStructure>();
     const [team, setTeam] = useState<Team | null>(null);
-    const [member, setMember] = useState<TeamMember | null>(null);
+    const [selectedMember, setSelectedMember] = useState<TeamMember | null>(null);
     const { menu, loading } = actions;
     const { user } = useContext(UserContext);
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+    };
 
     const getTeam = async (): Promise<void> => {
         const { data } = await api.getTeam(teamID);
 
-        setMember(data.members?.find((teamMember) => teamMember.id === member?.id) as TeamMember);
+        setSelectedMember(data.members?.find((teamMember) => teamMember.id === selectedMember?.id) as TeamMember);
         setTeam(data);
     }
 
@@ -46,7 +51,7 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
         setActions({ loading: true });
 
         try {
-            await api.removeMember(team?.id as string, member?.id as string);
+            await api.removeMember(team?.id as string, selectedMember?.id as string);
         } catch (error: any) {
             setActions({ loading: false });
             setError({
@@ -66,7 +71,7 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
         setActions({ loading: true });
 
         try {
-            await api.patchMember(team?.id as string, member?.id as string, {
+            await api.patchMember(team?.id as string, selectedMember?.id as string, {
                 permission: 1
             });
         } catch (error: any) {
@@ -88,7 +93,7 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
         setActions({ loading: true });
 
         try {
-            await api.patchMember(team?.id as string, member?.id as string, {
+            await api.patchMember(team?.id as string, selectedMember?.id as string, {
                 permission: 0
             });
         } catch (error: any) {
@@ -107,13 +112,13 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
     };
 
     const transferPosse = async (): Promise<void> => {
-        if (!team || !member || !team.members) return;
+        if (!team || !selectedMember || !team.members) return;
 
         setActions({ loading: true });
 
         try {
-            await api.transferOnwer(member.id);
-        } catch (error: any) {            
+            await api.transferOnwer(selectedMember.id);
+        } catch (error: any) {
             setActions({ loading: false });
             setError({
                 show: true,
@@ -138,80 +143,71 @@ export const ManageMembers: FC<{ color: Theme, updateAuditLogs: () => Promise<vo
         <section className="flex w-full items-center justify-center flex-col gap-2">
             <div className="flex flex-col w-full">
                 <span className="text-center font-bold text-2xl my-2">Gerenciar membros</span>
-                <div>
-                    <button onClick={() => {
-                        setActions({ menu: !actions.menu });
-                        setMember(null);
-                    }} className={`bg-neutral-900 p-3 items-center justify-center flex flex-row ${borderColor[color]} rounded-lg border-2 w-full`}>
-                        {member ? (
-                            <>
-                                <div className="flex items-center justify-start gap-2 w-full flex-grow">
-                                    <img className="rounded-full w-12" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
-                                    <span className="text-lg font-bold">{member.username}</span>
+                <div className="flex flex-col gap-3 w-full">
+                    <span className="text-lg font-bold">Procurar por um membro</span>
+                    <form className="w-full xl:w-full xl:h-14 flex flex-row">
+                        <div className="w-full">
+                            <input
+                                placeholder="Digite aqui o nome de algum membro"
+                                value={searchQuery}
+                                onChange={handleSearchInputChange}
+                                className={`bg-transparent disabled:opacity-5 focus:outline-none border-2 rounded-lg p-2 w-full h-14 ${borderColor[color]}`}
+                                type="text"
+                            />
+                        </div>
+                    </form>
+                </div>
+                {selectedMember && (
+                    <div className="p-3 w-full bg-neutral-800 rounded-lg mt-2">
+                        <div className="flex flex-row xl xl:flex-col xl:justify-center xl:w-full">
+                            <div className="flex items-center xl:flex-col justify-start gap-3 w-full flex-grow">
+                                <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${selectedMember.id}/${selectedMember.avatar}.png`} />
+                                <div className="flex flex-col gap-1 xl:text-center">
+                                    <div className="flex gap-2 items-center xl:flex-col">
+                                        <span className="text-lg font-bold">{selectedMember.username}</span>
+                                        <span className="text-neutral-500">({selectedMember.id})</span>
+                                    </div>
+                                    <span>Cargo <strong>{selectedMember?.permission === 1 ? "Membro" : selectedMember.permission === TeamPermissions.Owner ? "Dono" : "Administrador"}</strong></span>
                                 </div>
-                                <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${menu ? "rotate-180" : "rotate-0"}`} size={25} />
-                            </>
-                        ) : (
-                            <>
-                                <span className="flex flex-grow">Clique aqui para selecionar um membro para gerenciar.</span>
-                                <iconMD.MdOutlineKeyboardArrowDown className={`transition-all duration-300 ${menu ? "rotate-180" : "rotate-0"}`} size={25} />
-                            </>
-                        )}
-                    </button>
-                </div>
-                <div className="relative w-full">
-                    <div className={`${menu ? "opacity-100 visible" : "opacity-0 invisible"} transition-all duration-300 w-full flex items-center justify-center`}>
-                        {menu && (
-                            <div className={`bg-neutral-900 relative rounded-b-lg ${scrollBar[color]} overflow-auto max-h-[600px] w-[95%] ${borderColor[color]} border-2 border-t-0 flex items-center flex-col gap-2 p-3`}>
-                                {team?.members?.map((member, index) => (
-                                    <button key={index} onClick={() => {
-                                        setMember(member);
-                                        setActions({ menu: false });
-                                    }} className="flex xl:flex-col items-center justify-start w-full gap-3 p-3 transition-colors duration-300 hover:bg-neutral-800 rounded-lg">
-                                        <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
-                                        <span className="text-xl">{member.username}</span>
-                                        <span className="text-[#797979] items-center flex text-[13px] justify-center">
-                                            ( {member?.permission === 1 ? "Membro" : member.permission === TeamPermissions.Owner ? "Dono" : "Administrador"} )
-                                        </span>
-                                    </button>
-                                ))}
                             </div>
-                        )}
+                            <div className="flex flex-row xl:flex-col xl:justify-center xl:w-full gap-2 w-full p-2 justify-end">
+                                {youinTeam?.id !== selectedMember.id && (
+                                    ((youinTeam?.permission === TeamPermissions.Administrator || youinTeam?.permission === TeamPermissions.Owner) && selectedMember.permission !== TeamPermissions.Owner && (selectedMember.permission && youinTeam.permission === TeamPermissions.Administrator || TeamPermissions.Owner)) && (
+                                        <>
+                                            <Button action={kickMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Expulsar</span><iconBS.BsHammer /></Button>
+                                            {selectedMember?.permission === TeamPermissions.ReadOnly ? (
+                                                <Button action={promoveMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Promover</span><iconBS.BsArrowUp /></Button>
+                                            ) : (
+                                                <Button action={demoteMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Rebaixar</span><iconBS.BsArrowDown /></Button>
+                                            )}
+                                            {youinTeam?.id !== selectedMember.id && youinTeam?.permission === TeamPermissions.Owner && <Button action={transferPosse} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Transferir posse</span><iconBI.BiSolidCrown /></Button>}
+                                        </>
+                                    )
+                                )}
+                            </div>
+                        </div>
+                        {error?.show && <PopUpError setShow={setError} show={error} />}
                     </div>
-                </div>
+                )}
+                {(team && team.members) ? (
+                    <div className="flex flex-col gap-2 mt-2">
+                        {team.members.filter((member) => member.username.toLowerCase().includes(searchQuery.toLowerCase())).map((member) => (
+                            <button onClick={() => setSelectedMember(member)} className={`${member.id === selectedMember?.id ? "bg-neutral-700" : "bg-neutral-800"} flex transition duration-300 hover:bg-neutral-700 items-center p-3 rounded-lg gap-2`} key={member.id}>
+                                <img className="rounded-full w-12 h-12" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
+                                <div className="flex gap-3 flex-wrap">
+                                    <span className="text-xl">{member.username}</span>
+                                    <span className="text-[#797979] items-center flex text-[13px] justify-center">
+                                        ( {member?.permission === 1 ? "Membro" : member.permission === TeamPermissions.Owner ? "Dono" : "Administrador"} )
+                                    </span>
+                                </div>
+                            </button>
+                        ))}
+
+                    </div>
+                ) : (
+                    <div>Carregando...</div>
+                )}
             </div>
-            {member && (
-                <div className="p-3 w-full bg-neutral-800 rounded-lg">
-                    <div className="flex flex-row xl xl:flex-col xl:justify-center xl:w-full">
-                        <div className="flex items-center xl:flex-col justify-start gap-3 w-full flex-grow">
-                            <img className="rounded-full w-20" src={`https://cdn.discordapp.com/avatars/${member.id}/${member.avatar}.png`} />
-                            <div className="flex flex-col gap-1 xl:text-center">
-                                <div className="flex gap-2 items-center xl:flex-col">
-                                    <span className="text-lg font-bold">{member.username}</span>
-                                    <span className="text-neutral-500">({member.id})</span>
-                                </div>
-                                <span>Cargo <strong>{member?.permission === 1 ? "Membro" : member.permission === TeamPermissions.Owner ? "Dono" : "Administrador"}</strong></span>
-                            </div>
-                        </div>
-                        <div className="flex flex-row xl:flex-col xl:justify-center xl:w-full gap-2 w-full p-2 justify-end">
-                            {youinTeam?.id !== member.id && (
-                                ((youinTeam?.permission === TeamPermissions.Administrator || youinTeam?.permission === TeamPermissions.Owner) && member.permission !== TeamPermissions.Owner && (member.permission && youinTeam.permission === TeamPermissions.Administrator || TeamPermissions.Owner)) && (
-                                    <>
-                                        <Button action={kickMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Expulsar</span><iconBS.BsHammer /></Button>
-                                        {member?.permission === TeamPermissions.ReadOnly ? (
-                                            <Button action={promoveMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Promover</span><iconBS.BsArrowUp /></Button>
-                                        ) : (
-                                            <Button action={demoteMember} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Rebaixar</span><iconBS.BsArrowDown /></Button>
-                                        )}
-                                        {youinTeam?.id !== member.id && youinTeam?.permission === TeamPermissions.Owner && <Button action={transferPosse} disabled={loading} clas="flex items-center flex-row gap-3 xl:w-full xl:flex-row xl:justify-end"><span className="xl:flex xl:flex-grow xl:justify-center">Transferir posse</span><iconBI.BiSolidCrown /></Button>}
-                                    </>
-                                )
-                            )}
-                        </div>
-                    </div>
-                    {error?.show && <PopUpError setShow={setError} show={error} />}
-                </div>
-            )}
-        </section>
+        </section >
     )
 };
