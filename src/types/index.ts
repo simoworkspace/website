a21aeffab509eff22d04834ddfdac1fe0ba554aa
Caio <105593678+Spyei@users.noreply.github.com>
2023-12-16 17:50:01 -0300

    chore: typings name

--- src/types/index.ts
+++ src/types/index.ts
@@ -161,26 +161,6 @@ interface Locales {
     ko?: string;
 }
 
-/** Represents Discord user flags */
-
-enum UserFlags {
-    Staff = 1 << 0,
-    Partner = 1 << 1,
-    Hypesquad = 1 << 2,
-    BugHunterLevel1 = 1 << 3,
-    HypesquadOnlineHouse1 = 1 << 6,
-    HypesquadOnlineHouse2 = 1 << 7,
-    HypesquadOnlineHouse3 = 1 << 8,
-    PremiumEarlySupporter = 1 << 9,
-    TeamPseudoUser = 1 << 10,
-    BugHunterLevel2 = 1 << 14,
-    VerifiedBot = 1 << 16,
-    VerifiedDeveloper = 1 << 17,
-    CertifiedModeration = 1 << 18,
-    BotHTPPInteractions = 1 << 19,
-    ActiveDeveloper = 1 << 22
-}
-
 enum DiscordNitroType {
     None,
     NitroClassic,
@@ -318,6 +298,8 @@ export enum AuditLogActionType {
      * Invite was updated
      */
     InviteUpdate,
+    
+    MemberAutoKick
 }
 
 export type AnyAuditLogChange =
