9e10530505e526eba4ec9cb51fc7461d5b072f88
Spyei <caio.salamao1234@gmail.com>
2026-03-16 14:04:03 -0400

    feat: mock data

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -21,40 +21,44 @@ import { CreateTeamPage } from "../pages/Team/CreateTeam";
 import { TeamPage } from "../pages/Team/Team";
 import { ManageTeamPage } from "../pages/Team/ManageTeam";
 import { InvitePage } from "../pages/Team/Invite";
-import { Auth } from "../components/Mixed/Auth";
 import { Login } from "../pages/Login/Login";
 
 function App() {
-    const { color } = useContext(ThemeContext);
+	const { color } = useContext(ThemeContext);
 
-    return (
-        <main className={`xl:no-scrollbar xlr:h-1 flex flex-col items-center overflow-x-hidden min-h-screen ${appColor[color]} bg-fixed scrollbar-track-neutral-900 scrollbar-thin`}>
-            <Header/>
-            <section className="flex flex-1 flex-col">
-                <Routes>
-                    <Route path="/testes" element={<Tests />} />
-                    <Route path="/bot/:botid" element={<Bot />} />
-                    <Route path="/vote/:botid" element={<Vote />} />
-                    <Route path="/" element={<Bots />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/notifications" element={<NotificationsPage />} />
-                    <Route path="/themes" element={<ThemesPage />} />
-                    <Route path="/addbot" element={<Auth><Addbot /></Auth>} />
-                    <Route path="/user/:userid" element={<User />} />
-                    <Route path="/dashboard" element={<Auth><Dashboard /></Auth>} />
-                    <Route path="/dashboard/edit/:botId" element={<Auth><DashboardEditPage /></Auth>} />
-                    <Route path="/team/create" element={<Auth><CreateTeamPage /></Auth>} />
-                    <Route path="/team/:teamId" element={<TeamPage />} />
-                    <Route path="/team/:teamId/invite/:hash" element={<InvitePage />} />
-                    <Route path="/team/manage/:teamId" element={<Auth><ManageTeamPage /></Auth>} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-                <Mobilemenu />
-            </section>
-            <Footer />
-        </main>
-    );
+	return (
+		<main
+			className={`xl:no-scrollbar xlr:h-1 flex flex-col items-center overflow-x-hidden min-h-screen ${appColor[color]} bg-fixed scrollbar-track-neutral-900 scrollbar-thin`}
+		>
+			<Header />
+			<section className="flex flex-1 flex-col">
+				<Routes>
+					<Route path="/testes" element={<Tests />} />
+					<Route path="/bot/:botid" element={<Bot />} />
+					<Route path="/vote/:botid" element={<Vote />} />
+					<Route path="/" element={<Bots />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/notifications" element={<NotificationsPage />} />
+					<Route path="/themes" element={<ThemesPage />} />
+					<Route path="/addbot" element={<Addbot />} />
+					<Route path="/user/:userid" element={<User />} />
+					<Route path="/dashboard" element={<Dashboard />} />
+					<Route
+						path="/dashboard/edit/:botId"
+						element={<DashboardEditPage />}
+					/>
+					<Route path="/team/create" element={<CreateTeamPage />} />
+					<Route path="/team/:teamId" element={<TeamPage />} />
+					<Route path="/team/:teamId/invite/:hash" element={<InvitePage />} />
+					<Route path="/team/manage/:teamId" element={<ManageTeamPage />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+				<Mobilemenu />
+			</section>
+			<Footer />
+		</main>
+	);
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,24 +5,42 @@ import { InputSearch } from "../Search/InputSearch";
 import { Link } from "react-router-dom";
 import { NotificationButton } from "../Notification/Button";
 import { LoginMenu } from "../DropdownMenu/Menu";
+import { MdWarning } from "react-icons/md";
 
 export const Header: React.FC = () => {
-    const [inputSearch, setInputSearch] = useState<boolean>(false);
+	const [inputSearch, setInputSearch] = useState<boolean>(false);
 
-    return (
-        <>
-            <div className="sticky xl:fixed top-0 w-screen max-w-[1500px] z-10 backdrop-blur-sm bg-transparent h-[74px] xl:h-[60px] xl:border-b-2">
-                <div className="w-full flex justify-center items-center bg-transparent xl:flex-wrap xl:h-[60px]">
-                    <div className="flex xl:relative xl:w-screen xl:justify-center xl:items-center">
-                        <Link to="/" className="text-white flex flex-grow flex-row text-[32px] mx-10 my-3 xl:mx-0 xl:my-1 xl:justify-center xl:w-full xl:h-full xl:items-center xl:ml-6"><strong>Simo</strong></Link>
-                        <Button action={() => setInputSearch(!inputSearch)} clas="xlr:invisible hover:bg-transparent"><icon.BsSearch fill="#fff" /></Button>
-                    </div>
-                    <InputSearch show={inputSearch} />
-                    <NotificationButton />
-                    <LoginMenu/>
-                </div>
-            </div>
-            <hr className="w-screen xlr:fixed mt-[74px] xl:opacity-0 border-neutral-500" />
-        </>
-    );
+	return (
+		<>
+			<div className="sticky xl:fixed top-0 w-screen max-w-[1500px] z-10 backdrop-blur-sm bg-transparent h-[112px] xl:h-[112px] xl:border-b xl:border-neutral-500">
+				<div className="bg-yellow-300 w-full p-1 flex items-center justify-center text-center text-sm">
+					<MdWarning fill="#000" size={24} className="inline-block mx-2" />
+					<span>
+						Esse projeto foi descontinuado, todas informações do website são as
+						últimas da database, e são estáticas.
+					</span>
+				</div>
+				<div className="w-full flex justify-center items-center bg-transparent xl:flex-wrap xl:h-[60px]">
+					<div className="flex xl:relative xl:w-screen xl:justify-center xl:items-center">
+						<Link
+							to="/"
+							className="text-white flex flex-grow flex-row text-[32px] mx-10 my-3 xl:mx-0 xl:my-1 xl:justify-center xl:w-full xl:h-full xl:items-center xl:ml-6"
+						>
+							<strong>Simo</strong>
+						</Link>
+						<Button
+							action={() => setInputSearch(!inputSearch)}
+							clas="xlr:invisible hover:bg-transparent"
+						>
+							<icon.BsSearch fill="#fff" />
+						</Button>
+					</div>
+					<InputSearch show={inputSearch} />
+					<NotificationButton />
+					<LoginMenu />
+				</div>
+			</div>
+			<hr className="w-screen xlr:fixed mt-[112px] xl:opacity-0 border-neutral-500" />
+		</>
+	);
 };
